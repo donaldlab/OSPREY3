5f94dad784ad0b00490882d5a03e67f896176d25
Adegoke Ojewole <MCIC@wl-10-190-91-226.wireless.duke.edu>
2016-07-30 18:17:38 -0400

    fixed k star test

--- tests/edu/duke/cs/osprey/kstar/TestKStar.java
+++ tests/edu/duke/cs/osprey/kstar/TestKStar.java
@@ -1,5 +1,7 @@
 package edu.duke.cs.osprey.kstar;
 
+import org.junit.Test;
+
 import edu.duke.cs.osprey.control.KStarCalculator;
 import edu.duke.cs.osprey.energy.MultiTermEnergyFunction;
 import edu.duke.cs.osprey.kstar.KSConfigFileParser;
@@ -7,15 +9,16 @@ import edu.duke.cs.osprey.parallelism.ThreadParallelism;
 import junit.framework.TestCase;
 import junit.framework.TestResult;
 
-public class KStarTests extends TestCase {
+public class TestKStar {
 
 	KStarCalculator ksc;
 	KSConfigFileParser cfp;
 
-	public static void main(String[] args) {
+	@Test
+	public void test2RL0() {
 		try {
 			
-			KStarTests tks = new KStarTests();
+			TestKStar tks = new TestKStar();
 			tks.setUp();
 			tks.run();
 			
@@ -29,8 +32,6 @@ public class KStarTests extends TestCase {
 	}
 
 	protected void setUp() throws Exception {
-		
-		super.setUp();
 		
 		String[] testArgs = new String[]{"-c", "test/2RL0.kstar/cfgKStar.txt", 
 				"Dummy command", "test/2RL0.kstar/cfgMutSearch.txt", "test/2RL0.kstar/cfgSystem.txt"};
