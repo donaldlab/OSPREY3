5fa31e3f42c111052459630d20114026492678b7
Adegoke Ojewole <MCIC@wl-10-190-89-7.wireless.duke.edu>
2016-08-11 11:16:46 -0400

    speed up kstar test, added parallel0 implementation to factory

--- tests/edu/duke/cs/osprey/kstar/TestKStar.java
+++ tests/edu/duke/cs/osprey/kstar/TestKStar.java
@@ -6,7 +6,6 @@ import edu.duke.cs.osprey.control.KStarCalculator;
 import edu.duke.cs.osprey.energy.MultiTermEnergyFunction;
 import edu.duke.cs.osprey.kstar.KSConfigFileParser;
 import edu.duke.cs.osprey.parallelism.ThreadParallelism;
-import junit.framework.TestCase;
 import junit.framework.TestResult;
 
 public class TestKStar {
@@ -37,6 +36,9 @@ public class TestKStar {
 				"Dummy command", "test/2RL0.kstar/cfgMutSearch.txt", "test/2RL0.kstar/cfgSystem.txt"};
 		cfp = new KSConfigFileParser(testArgs);//args 1, 3+ are configuration files
 		cfp.loadData();
+		cfp.getParams().setValue("KSTARPFUNCMETHOD", "parallel2");
+		cfp.getParams().setValue("KSTARPFUNCTHREADS", "3");
+		cfp.getParams().setValue("NUMTHREADS", "3");
 		
 		ThreadParallelism.setNumThreads(cfp.getParams().getInt("numThreads", ThreadParallelism.getNumThreads()));
 		MultiTermEnergyFunction.setNumThreads(ThreadParallelism.getNumThreads());
