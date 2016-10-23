b440d4009c899d656ed95588d98091022ee25d26
Jeff Martin <cuchaz@gmail.com>
2016-10-23 14:20:46 -0400

    test out dynamic parallelism
    preliminary tests show device-side kernel launches could be up to 3.5
    times faster than doing all kernel launches on the host side.
    
    pretty promising, so it's worth implementing the full forcefield kernel
    in CUDA I think

--- tests/edu/duke/cs/osprey/gpu/cuda/CudaPlayground.java
+++ tests/edu/duke/cs/osprey/gpu/cuda/CudaPlayground.java
@@ -8,13 +8,18 @@ import edu.duke.cs.osprey.tools.TimeFormatter;
 import jcuda.Pointer;
 import jcuda.Sizeof;
 import jcuda.driver.CUcontext;
-import jcuda.driver.CUdevice;
 import jcuda.driver.CUdeviceptr;
 import jcuda.driver.CUfunction;
 import jcuda.driver.CUmodule;
 import jcuda.driver.JCudaDriver;
 
 public class CudaPlayground {
+	
+	private static final int NumElements = 10000;
+	private static final int NumRuns = 10;
+	
+	private static final int BlockThreads = 256;
+	private static final int GridBlocks = 40;//(NumElements + BlockThreads - 1)/BlockThreads;
 	
 	public static void main(String[] args)
 	throws Exception {
@@ -25,26 +30,29 @@ public class CudaPlayground {
 		// info on dynamic parallelism:
 		// http://docs.nvidia.com/cuda/cuda-c-programming-guide/#cuda-dynamic-parallelism
 		
-		JCudaDriver.setExceptionsEnabled(true);
-		
 		// init CUDA
-		JCudaDriver.cuInit(0);
-		CUdevice device = new CUdevice();
-		JCudaDriver.cuDeviceGet(device, 0);
-		CUcontext context = new CUcontext();
-		JCudaDriver.cuCtxCreate(context, 0, device);
+		Gpu gpu = Gpus.get().getGpus().get(0);
+		CUcontext context = gpu.makeContextForThisThread();
 		
 		// load the kernel
-		File kernelFile = ResourceExtractor.extract("kernelSource/test.ptx", Kernel.class);
+		File kernelFile = ResourceExtractor.extract("kernelBinaries/test.cubin", Gpu.class);
 		CUmodule module = new CUmodule();
 		JCudaDriver.cuModuleLoad(module, kernelFile.getAbsolutePath());
+		
+		hostLoop(module);
+		deviceLoop(module);
+		
+		JCudaDriver.cuModuleUnload(module);
+		JCudaDriver.cuCtxDestroy(context);
+	}
+	
+	private static void hostLoop(CUmodule module) {
 		
 		// get the kernel func
 		CUfunction func = new CUfunction();
 		JCudaDriver.cuModuleGetFunction(func, module, "add");
 		
 		// init host buffers
-		final int NumElements = 2000;
 		float[] a = new float[NumElements];
 		float[] b = new float[NumElements];
 		for (int i=0; i<NumElements; i++) {
@@ -68,45 +76,31 @@ public class CudaPlayground {
 			Pointer.to(pdB),
 			Pointer.to(pdOut)
 		);
-		int blockSizeX = 256;
-		int gridSizeX = (NumElements + blockSizeX - 1)/blockSizeX;
 		
-		final int NumRuns = 1000;
-		//Profiler p = new Profiler();
 		Stopwatch stopwatch = new Stopwatch().start();
 		
 		for (int i=0; i<NumRuns; i++) {
 		
-			//p.start("upload");
-		
 			// upload inputs
-			//JCudaDriver.cuMemcpyHtoDAsync(pdA, Pointer.to(a), NumElements*Sizeof.FLOAT, stream);
-			JCudaDriver.cuMemcpyHtoD(pdA, Pointer.to(a), NumElements*Sizeof.FLOAT);
-			JCudaDriver.cuMemcpyHtoD(pdB, Pointer.to(b), NumElements*Sizeof.FLOAT);
-			
-			//p.start("kernel");
+			JCudaDriver.cuMemcpyHtoDAsync(pdA, Pointer.to(a), NumElements*Sizeof.FLOAT, null);
+			JCudaDriver.cuMemcpyHtoDAsync(pdB, Pointer.to(b), NumElements*Sizeof.FLOAT, null);
 			
 			// launch the kernel
 			JCudaDriver.cuLaunchKernel(
 				func,
-				gridSizeX, 1, 1,
-				blockSizeX, 1, 1,
+				GridBlocks, 1, 1,
+				BlockThreads, 1, 1,
 				0,
 				null,
 				pParams,
 				null
 			);
-			JCudaDriver.cuCtxSynchronize();
-			
-			//p.start("download");
 			
 			// download the output
 			JCudaDriver.cuMemcpyDtoH(Pointer.to(out), pdOut, NumElements*Sizeof.FLOAT);
-			
-			//p.stop();
 		}
-		//System.out.println(p.makeReport(TimeUnit.MICROSECONDS));
 		stopwatch.stop();
+		System.out.println("total time: " + TimeFormatter.format(stopwatch.getTimeNs(), 1));
 		System.out.println("avg time per op: " + TimeFormatter.format(stopwatch.getTimeNs()/NumRuns, 1));
 		
 		System.out.println("sum");
@@ -118,6 +112,71 @@ public class CudaPlayground {
 		JCudaDriver.cuMemFree(pdA);
 		JCudaDriver.cuMemFree(pdB);
 		JCudaDriver.cuMemFree(pdOut);
-		JCudaDriver.cuModuleUnload(module);
+	}
+	
+	private static void deviceLoop(CUmodule module) {
+		
+		// get the kernel func
+		CUfunction func = new CUfunction();
+		JCudaDriver.cuModuleGetFunction(func, module, "loop");
+		
+		// init host buffers
+		float[] a = new float[NumElements];
+		float[] b = new float[NumElements];
+		for (int i=0; i<NumElements; i++) {
+			a[i] = (float)i;
+			b[i] = (float)i;
+		}
+		float[] out = new float[NumElements];
+		
+		// allocate buffers
+		CUdeviceptr pdA = new CUdeviceptr();
+		JCudaDriver.cuMemAlloc(pdA, NumElements*Sizeof.FLOAT);
+		CUdeviceptr pdB = new CUdeviceptr();
+		JCudaDriver.cuMemAlloc(pdB, NumElements*Sizeof.FLOAT);
+		CUdeviceptr pdOut = new CUdeviceptr();
+		JCudaDriver.cuMemAlloc(pdOut, NumElements*Sizeof.FLOAT);
+		
+		// prep kernel args
+		Pointer pParams = Pointer.to(
+			Pointer.to(new int[] {NumElements}),
+			Pointer.to(pdA),
+			Pointer.to(pdB),
+			Pointer.to(pdOut)
+		);
+		
+		Stopwatch stopwatch = new Stopwatch().start();
+		
+		// upload inputs
+		JCudaDriver.cuMemcpyHtoDAsync(pdA, Pointer.to(a), NumElements*Sizeof.FLOAT, null);
+		JCudaDriver.cuMemcpyHtoDAsync(pdB, Pointer.to(b), NumElements*Sizeof.FLOAT, null);
+		
+		// launch the kernel
+		JCudaDriver.cuLaunchKernel(
+			func,
+			1, 1, 1,
+			1, 1, 1,
+			0,
+			null,
+			pParams,
+			null
+		);
+		
+		// download the output
+		JCudaDriver.cuMemcpyDtoH(Pointer.to(out), pdOut, NumElements*Sizeof.FLOAT);
+		
+		stopwatch.stop();
+		System.out.println("total time: " + TimeFormatter.format(stopwatch.getTimeNs(), 1));
+		System.out.println("avg time per op: " + TimeFormatter.format(stopwatch.getTimeNs()/NumRuns, 1));
+		
+		System.out.println("sum");
+		for (int i=0; i<10; i++) {
+			System.out.println(out[i]);
+		}
+		
+		// cleanup
+		JCudaDriver.cuMemFree(pdA);
+		JCudaDriver.cuMemFree(pdB);
+		JCudaDriver.cuMemFree(pdOut);
 	}
 }
