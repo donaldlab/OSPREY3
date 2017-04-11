d7260ee27b8b8a86ef3777f0599b0aca8238e25c
Jeff Martin <cuchaz@gmail.com>
2017-04-11 13:06:08 -0400

    optimize ResidueForcefieldEnergy a bit more
    it's like 28% faster than MultiTermEnergyFunction now

--- src/edu/duke/cs/osprey/energy/forcefield/ResidueForcefieldEnergy.java
+++ src/edu/duke/cs/osprey/energy/forcefield/ResidueForcefieldEnergy.java
@@ -13,9 +13,8 @@ import edu.duke.cs.osprey.energy.forcefield.ForcefieldParams.NBParams;
 import edu.duke.cs.osprey.energy.forcefield.ForcefieldParams.SolvationForcefield;
 import edu.duke.cs.osprey.structure.Atom;
 import edu.duke.cs.osprey.structure.AtomConnectivity;
-import edu.duke.cs.osprey.structure.AtomConnectivity.AtomPairList;
+import edu.duke.cs.osprey.structure.AtomConnectivity.AtomPairs;
 import edu.duke.cs.osprey.structure.AtomNeighbors;
-import edu.duke.cs.osprey.structure.AtomNeighbors.Type;
 import edu.duke.cs.osprey.structure.Molecule;
 import edu.duke.cs.osprey.structure.Residue;
 
@@ -37,8 +36,9 @@ public class ResidueForcefieldEnergy implements EnergyFunction.DecomposableByDof
 		public double offset = 0;
 		public int numAtomPairs = 0;
 		
-		// layout per atom pair: atom1Offset, atom2Offset, flags (is14Bonded, isHeavyPair)
-		public byte[] flags = null;
+		// layout per atom pair: flags (bit is14Bonded, bit isHeavyPair, 6 bits space, 3 bytes space, short atom1Offset, short atom2Offset)
+		// yeah, there's some extra space here, but keeping 8-byte alignments is fast on 64 bit machines
+		public long[] flags = null;
 		
 		// layout per atom pair: charge, Aij, Bij, radius1, lambda1, alpha1, radius2, lambda2, alpha2
 		public double[] precomputed = null;
@@ -192,87 +192,83 @@ public class ResidueForcefieldEnergy implements EnergyFunction.DecomposableByDof
 		// build the atom pairs
 		for (ResPair pair : resPairs) {
 			
-			// count the number of atom pairs
-			AtomPairList atomPairs = connectivity.getAtomPairs(mol, pair.res1, pair.res2);
+			AtomPairs atomPairs = connectivity.getAtomPairs(mol, pair.res1, pair.res2);
+			
+			// count the number of atom pairs and allocate space 
 			pair.numAtomPairs = atomPairs.getNumPairs(AtomNeighbors.Type.BONDED14) + atomPairs.getNumPairs(AtomNeighbors.Type.NONBONDED);
-			pair.flags = new byte[pair.numAtomPairs*3];
+			pair.flags = new long[pair.numAtomPairs];
 			pair.precomputed = new double[pair.numAtomPairs*9];
 			
 			int flagsIndex = 0;
 			int precomputedIndex = 0;
 			
-			for (int i=0; i<atomPairs.size(); i++) {
-		
-				// we only care about 1-4 bonded and non-bonded pairs
-				AtomNeighbors.Type atomPairType = atomPairs.getType(i);
-				if (atomPairType != AtomNeighbors.Type.BONDED14 && atomPairType != AtomNeighbors.Type.NONBONDED) {
-					continue;
-				}
-				
-				// set atoms indices
-				// residues shouldn't have more than 127 atoms, right?
-				int atomIndex1 = atomPairs.getIndex1(pair.res1, pair.res2, i);
-				int atomIndex2 = atomPairs.getIndex2(pair.res1, pair.res2, i);
-				pair.flags[flagsIndex++] = (byte)atomIndex1;
-				pair.flags[flagsIndex++] = (byte)atomIndex2;
-				
-				Atom atom1 = pair.res1.atoms.get(atomIndex1);
-				Atom atom2 = pair.res2.atoms.get(atomIndex2);
-				
-				// set the flags
-				boolean isHeavyPair = !atom1.isHydrogen() && !atom2.isHydrogen();
-				boolean is14Bonded = atomPairType == AtomNeighbors.Type.BONDED14;
-				int flags = 0
-					| (isHeavyPair ? 0x1 : 0)
-					| (is14Bonded ? 0x2 : 0);
-				pair.flags[flagsIndex++] = (byte)flags;
-				
-				// calc electrostatics params
-				pair.precomputed[precomputedIndex++] = atom1.charge*atom2.charge;
-				
-				// calc vdW params
-				// Aij = (ri+rj)^12 * sqrt(ei*ej)
-				// Bij = (ri+rj)^6 * sqrt(ei*ej)
-				
-				getNonBondedParams(atom1, nbparams1);
-				getNonBondedParams(atom2, nbparams2);
-				double epsilon = Math.sqrt(nbparams1.epsilon*nbparams2.epsilon);
-				double radiusSum = nbparams1.r + nbparams2.r;
-				double Bij = radiusSum*radiusSum;
-				Bij = Bij*Bij*Bij;
-				double Aij = Bij*Bij;
-				Aij *= epsilon*Amult;
-				Bij *= epsilon*Bmult;
-				
-				// vdW scaling by connectivity
-				if (atomPairType == Type.BONDED14) {
-					Aij *= params.forcefld.Aij14Factor;
-					Bij *= params.forcefld.Bij14Factor;
-				} else if (atomPairType == Type.NONBONDED) {
-					Bij *= 2;
-				}
-				
-				pair.precomputed[precomputedIndex++] = Aij;
-				pair.precomputed[precomputedIndex++] = Bij;
-				
-				// compute solvation if needed
-				if (isHeavyPair) {
+			for (AtomNeighbors.Type type : Arrays.asList(AtomNeighbors.Type.BONDED14, AtomNeighbors.Type.NONBONDED)) {
+				for (int[] atomPair : atomPairs.getPairs(type)) {
+			
+					Atom atom1 = pair.res1.atoms.get(atomPair[0]);
+					Atom atom2 = pair.res2.atoms.get(atomPair[1]);
 					
-					getSolvParams(atom1, solvparams1);
-					getSolvParams(atom2, solvparams2);
-					double alpha1 = solvCoeff*solvparams1.dGfree*solvparams2.volume/solvparams1.lambda;
-					double alpha2 = solvCoeff*solvparams2.dGfree*solvparams1.volume/solvparams2.lambda;
+					// pack the flags
+					boolean isHeavyPair = !atom1.isHydrogen() && !atom2.isHydrogen();
+					boolean is14Bonded = type == AtomNeighbors.Type.BONDED14;
+					int atomOffset1 = atomPair[0]*3;
+					int atomOffset2 = atomPair[1]*3;
+					long flags = is14Bonded ? 1 : 0;
+					flags <<= 1;
+					flags |= isHeavyPair ? 1 : 0;
+					flags <<= 46;
+					flags |= (atomOffset1) & 0xffffL;
+					flags <<= 16;
+					flags |= (atomOffset2) & 0xffffL;
+					pair.flags[flagsIndex++] = flags;
 					
-					pair.precomputed[precomputedIndex++] = solvparams1.radius;
-					pair.precomputed[precomputedIndex++] = solvparams1.lambda;
-					pair.precomputed[precomputedIndex++] = alpha1;
-					pair.precomputed[precomputedIndex++] = solvparams2.radius;
-					pair.precomputed[precomputedIndex++] = solvparams2.lambda;
-					pair.precomputed[precomputedIndex++] = alpha2;
+					// calc electrostatics params
+					pair.precomputed[precomputedIndex++] = atom1.charge*atom2.charge;
 					
-				} else {
+					// calc vdW params
+					// Aij = (ri+rj)^12 * sqrt(ei*ej)
+					// Bij = (ri+rj)^6 * sqrt(ei*ej)
 					
-					precomputedIndex += 6;
+					getNonBondedParams(atom1, nbparams1);
+					getNonBondedParams(atom2, nbparams2);
+					double epsilon = Math.sqrt(nbparams1.epsilon*nbparams2.epsilon);
+					double radiusSum = nbparams1.r + nbparams2.r;
+					double Bij = radiusSum*radiusSum;
+					Bij = Bij*Bij*Bij;
+					double Aij = Bij*Bij;
+					Aij *= epsilon*Amult;
+					Bij *= epsilon*Bmult;
+					
+					// vdW scaling by connectivity
+					if (is14Bonded) {
+						Aij *= params.forcefld.Aij14Factor;
+						Bij *= params.forcefld.Bij14Factor;
+					} else {
+						Bij *= 2;
+					}
+					
+					pair.precomputed[precomputedIndex++] = Aij;
+					pair.precomputed[precomputedIndex++] = Bij;
+					
+					// compute solvation params if needed
+					if (isHeavyPair) {
+						
+						getSolvParams(atom1, solvparams1);
+						getSolvParams(atom2, solvparams2);
+						double alpha1 = solvCoeff*solvparams1.dGfree*solvparams2.volume/solvparams1.lambda;
+						double alpha2 = solvCoeff*solvparams2.dGfree*solvparams1.volume/solvparams2.lambda;
+						
+						pair.precomputed[precomputedIndex++] = solvparams1.radius;
+						pair.precomputed[precomputedIndex++] = solvparams1.lambda;
+						pair.precomputed[precomputedIndex++] = alpha1;
+						pair.precomputed[precomputedIndex++] = solvparams2.radius;
+						pair.precomputed[precomputedIndex++] = solvparams2.lambda;
+						pair.precomputed[precomputedIndex++] = alpha2;
+						
+					} else {
+						
+						precomputedIndex += 6;
+					}
 				}
 			}
 			
@@ -366,31 +362,38 @@ public class ResidueForcefieldEnergy implements EnergyFunction.DecomposableByDof
 			ResPair pair = resPairs[i];
 			
 			// copy pair values/references to the stack/registers
-			// so we don't have to touch the pair object inside the atom pair loop
+			// so we don't have to touch the pair object while inside the atom pair loop
 			double[] coords1 = pair.res1.coords;
 			double[] coords2 = pair.res2.coords;
 			int numAtomPairs = pair.numAtomPairs;
-			byte[] flags = pair.flags;
+			long[] flags = pair.flags;
 			double[] precomputed = pair.precomputed;
-			
-			int flagsIndex = 0;
-			int precomputedIndex = 0;
 			
 			double resPairEnergy = 0;
 			
 			// for each atom pair...
+			int j9 = 0;
 			for (int j=0; j<numAtomPairs; j++) {
+				
+				// read the flags
+				// NOTE: this is efficient, but destructive to the val
+				long atomPairFlags = flags[j];
+				int atomOffset2 = (int)(atomPairFlags & 0xffff);
+				atomPairFlags >>= 16;
+				int atomOffset1 = (int)(atomPairFlags & 0xffff);
+				atomPairFlags >>= 46;
+				boolean isHeavyPair = (atomPairFlags & 0x1) == 0x1;
+				atomPairFlags >>= 1;
+				boolean is14Bonded = (atomPairFlags & 0x1) == 0x1;
 				
 				// get the radius
 				double r2;
 				double r;
 				{
 					// read atom coords
-					int atomOffset1 = flags[flagsIndex++]*3;
 					double x1 = coords1[atomOffset1];
 					double y1 = coords1[atomOffset1 + 1];
 					double z1 = coords1[atomOffset1 + 2];
-					int atomOffset2 = flags[flagsIndex++]*3;
 					double x2 = coords2[atomOffset2];
 					double y2 = coords2[atomOffset2 + 1];
 					double z2 = coords2[atomOffset2 + 2];
@@ -406,17 +409,11 @@ public class ResidueForcefieldEnergy implements EnergyFunction.DecomposableByDof
 					r = Math.sqrt(r2);
 				}
 				
-				// read the atom pair flags
-				byte atomPairFlags = flags[flagsIndex++];
-				boolean isHeavyPair = (atomPairFlags & 0x1) == 0x1;
-				boolean is14Bonded = (atomPairFlags & 0x2) == 0x2;
-				
-				boolean doSolv = useEEF1 && isHeavyPair && r2 < solvCutoff2;
-				boolean doEs = isHeavyPair || useHEs;
+				// read the bit flags
 				
 				// electrostatics
-				if (doEs) {
-					double charge = precomputed[precomputedIndex++];
+				if (isHeavyPair || useHEs) {
+					double charge = precomputed[j9++];
 					if (is14Bonded) {
 						if (distDepDielect) {
 							resPairEnergy += scaledCoulombFactor*charge/r2;
@@ -431,14 +428,14 @@ public class ResidueForcefieldEnergy implements EnergyFunction.DecomposableByDof
 						}
 					}
 				} else {
-					precomputedIndex++;
+					j9++;
 				}
 				
 				// van der Waals
 				if (isHeavyPair || useHvdW) {
 					
-					double Aij = precomputed[precomputedIndex++];
-					double Bij = precomputed[precomputedIndex++];
+					double Aij = precomputed[j9++];
+					double Bij = precomputed[j9++];
 					
 					// compute vdw
 					double r6 = r2*r2*r2;
@@ -446,18 +443,18 @@ public class ResidueForcefieldEnergy implements EnergyFunction.DecomposableByDof
 					resPairEnergy += Aij/r12 - Bij/r6;
 					
 				} else {
-					precomputedIndex += 2;
+					j9 += 2;
 				}
 				
 				// solvation
-				if (doSolv) {
+				if (useEEF1 && isHeavyPair && r2 < solvCutoff2) {
 							
-					double radius1 = precomputed[precomputedIndex++];
-					double lambda1 = precomputed[precomputedIndex++];
-					double alpha1 = precomputed[precomputedIndex++];
-					double radius2 = precomputed[precomputedIndex++];
-					double lambda2 = precomputed[precomputedIndex++];
-					double alpha2 = precomputed[precomputedIndex++];
+					double radius1 = precomputed[j9++];
+					double lambda1 = precomputed[j9++];
+					double alpha1 = precomputed[j9++];
+					double radius2 = precomputed[j9++];
+					double lambda2 = precomputed[j9++];
+					double alpha2 = precomputed[j9++];
 					
 					// compute solvation energy
 					double Xij = (r - radius1)/lambda1;
@@ -465,17 +462,13 @@ public class ResidueForcefieldEnergy implements EnergyFunction.DecomposableByDof
 					resPairEnergy -= (alpha1*Math.exp(-Xij*Xij) + alpha2*Math.exp(-Xji*Xji))/r2;
 					
 				} else {
-					precomputedIndex += 6;
+					j9 += 6;
 				}
 			}
 			
 			// apply weights and offsets
 			energy += resPairEnergy*pair.weight;
 			energy += pair.offset;
-			
-			// just in case...
-			assert (flagsIndex == pair.flags.length);
-			assert (precomputedIndex == pair.precomputed.length);
 		}
 		
 		return energy;
