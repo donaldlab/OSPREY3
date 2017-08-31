202a0fdcdeedfe35dcacd6fb697e3fa6f7cbb477
gokeO <>
2017-08-31 14:29:05 -0400

    added more config file parsing code

---
+++ src/edu/duke/cs/osprey/EWAKStar/EWAKRatios.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.duke.cs.osprey.EWAKStar;
+
+import edu.duke.cs.osprey.confspace.SearchProblem;
+import edu.duke.cs.osprey.control.ConfigFileParser;
+
+/**
+ *
+ * @author lowegard
+ */
+public class EWAKRatios {
+    
+    private SearchProblem[] search;
+    
+    public EWAKRatios(ConfigFileParser cfp) {
+        
+    }
+    
+    public void run() {
+    };
+    
+}
