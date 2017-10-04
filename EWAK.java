7887e2297470a169d9c7912248b82d5e625b9552
Anna Lowegard <alowegard@utexas.edu>
2017-10-04 13:19:15 -0400

    Add files via upload
    Creating complex partition dictionary

---
+++ EWAK.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.duke.cs.osprey.ewakstar;
+
+/**
+*
+* @author Anna Lowegard(anna.lowegard@duke.edu)
+* Adegoke Ojewole (ao68@duke.edu)
+*/
+public class EWAK {
+    
+}
