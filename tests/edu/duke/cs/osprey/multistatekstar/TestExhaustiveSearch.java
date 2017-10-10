903e01dc0d3578562a8be15e928ef14f66ad24f0
Adegoke Ojewole <MCIC@Adegokes-MBP.fios-router.home>
2017-10-09 22:42:12 -0400

    added mskstar test case

---
+++ tests/edu/duke/cs/osprey/multistatekstar/TestExhaustiveSearch.java
@@ -0,0 +1,27 @@
+package edu.duke.cs.osprey.multistatekstar;
+
+import org.junit.Test;
+
+import edu.duke.cs.osprey.control.EnvironmentVars;
+import edu.duke.cs.osprey.control.MSKStarDoer;
+
+public class TestExhaustiveSearch {
+
+	@Test
+    public void test2RL0NegativeDesign() {
+		
+		String[] args = new String[] {
+				"-c", 
+				"test/2RL0-neg-des.mskstar/cfgKStar.txt",
+				"doMSKStar",
+				"test/2RL0-neg-des.mskstar/cfgSystem.txt",
+				"test/2RL0-neg-des.mskstar/multistate-spec-exhaustive.txt"
+	        };
+		
+		EnvironmentVars.setDataDir("dataFiles");
+		
+		MSKStarDoer msksd = new MSKStarDoer(args);
+		msksd.calcBestSequences();
+	}
+	
+}
