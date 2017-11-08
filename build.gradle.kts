4ecfaa762e576057a8260e552a96283b0da06178
Jeff Martin <cuchaz@gmail.com>
2017-11-08 17:50:00 -0500

    add gradle tasks to build CUDA kernels

--- build.gradle.kts
+++ build.gradle.kts
@@ -177,13 +177,20 @@ tasks {
 	distTar.enabled = false
 	pythonDistTar.enabled = false
 
+	val compileCuda_residueForcefield by creating(Exec::class) {
+		nvcc(this, "residueForcefield")
+	}
+
+	val compileCuda_residueCcd by creating(Exec::class) {
+		nvcc(this, "residueCcd", maxRegisters=64)
+	}
+
 	val compileCuda by creating {
-		description = "TODO"
-		doLast {
-			// see comments resources/gpuKernels/cuda/*.cu for nvcc invocations
-			// TODO: implement me
-			throw UnsupportedOperationException()
-		}
+		description = "Compile cuda kernels"
+		dependsOn(
+			compileCuda_residueForcefield,
+			compileCuda_residueCcd
+		)
 	}
 
 	val cleanDoc by creating(Delete::class) {
@@ -318,4 +325,39 @@ tasks {
 
 	// insert some build steps before we build the python dist
 	pythonDistZip.dependsOn(pythonWheel, makeDoc, pythonInstallScripts)
+}
+
+fun nvcc(exec: Exec, kernelName: String, maxRegisters: Int? = null, profile: Boolean = false) {
+
+	val args = mutableListOf("nvcc")
+
+	if (profile) {
+		// if profiling, compile for one arch with profiling/debug info
+		// NOTE: change this to your GPU's arch
+		args.addAll(listOf("-cubin", "-gencode=arch=compute_61,code=sm_61", "-lineinfo", "--ptxas-options=-v"))
+	} else {
+		// otherwise, compile for all archs
+		// see Maxwell compatibility guide:
+		// http://docs.nvidia.com/cuda/maxwell-compatibility-guide/index.html#building-maxwell-compatible-apps-using-cuda-6-0
+		args.addAll(listOf("-fatbin",
+			"-gencode=arch=compute_20,code=sm_20",
+			"-gencode=arch=compute_30,code=sm_30",
+			"-gencode=arch=compute_35,code=sm_35",
+			"-gencode=arch=compute_50,code=sm_50",
+			"-gencode=arch=compute_52,code=sm_52",
+			"-gencode=arch=compute_60,code=sm_60",
+			"-gencode=arch=compute_61,code=sm_61",
+			"-gencode=arch=compute_62,code=sm_62",
+			"-gencode=arch=compute_62,code=compute_62"
+		))
+	}
+
+	if (maxRegisters != null) {
+		args.addAll(listOf("-maxrregcount", "$maxRegisters"))
+	}
+
+	args.addAll(listOf("$kernelName.cu", "-o", "$kernelName.bin"))
+
+	exec.workingDir = file("resources/gpuKernels/cuda")
+	exec.commandLine(args)
 }
