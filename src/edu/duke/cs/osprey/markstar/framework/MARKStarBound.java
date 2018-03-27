22de3d364747621f0004dfcf01b26eb645d8e1b8
Graham <graham.t.holt@gmail.com>
2018-03-27 16:57:41 -0400

    basic methods and structure

--- src/edu/duke/cs/osprey/markstar/framework/MARKStarBound.java
+++ src/edu/duke/cs/osprey/markstar/framework/MARKStarBound.java
@@ -1,4 +1,41 @@
 package edu.duke.cs.osprey.markstar.framework;
 
+import edu.duke.cs.osprey.markstar.MARKStar;
+
+import java.util.PriorityQueue;
+
 public class MARKStarBound {
+    private MARKStarNode curMNode;
+    private PriorityQueue<MARKStarNode> queue;
+
+    public MARKStarBound(){
+        this.curMNode = new MARKStarNode();
+        this.queue = new PriorityQueue<MARKStarNode>();
+    }
+
+    public MARKStarBound(MARKStarNode root){
+        this.curMNode = root;
+        this.queue = new PriorityQueue<MARKStarNode>();
+    }
+
+    public double getBound(){
+        //TODO: implement method
+        /*
+        Pseudocode:
+        computeUpperBound()
+        computeLowerBound()
+        return uBound -lBound
+         */
+        return Double.NaN;
+    }
+
+    public void tightenBound(){
+        //TODO: implement method
+        /*
+        Pseudocode:
+        curMNode = queue.pop();
+        expand(curMNode)
+        curMNode.updateBounds();
+         */
+    }
 }
--- src/edu/duke/cs/osprey/markstar/framework/MARKStarNode.java
+++ src/edu/duke/cs/osprey/markstar/framework/MARKStarNode.java
@@ -1,4 +1,60 @@
 package edu.duke.cs.osprey.markstar.framework;
 
-public class MARKStarNode {
+import java.util.PriorityQueue;
+
+public class MARKStarNode implements Comparable<MARKStarNode> {
+    private double upperBound;
+    private double lowerBound;
+    private PriorityQueue<MARKStarNode> children; // TODO: Pick appropriate data structure
+
+    public MARKStarNode(){
+        this.upperBound = Double.NaN;
+        this.lowerBound = Double.NaN;
+        this.children = new PriorityQueue<MARKStarNode>();
+    }
+
+    public MARKStarNode(double upperBound, double lowerBound, PriorityQueue<MARKStarNode> children){
+        this.upperBound = upperBound;
+        this.lowerBound = lowerBound;
+        this.children = children;
+    }
+
+    public double getLowerBound(){
+        return this.lowerBound;
+    }
+
+    public double getUpperBound(){
+        return this.upperBound;
+    }
+
+    public PriorityQueue<MARKStarNode> getChildren(){
+        return this.children;
+    }
+
+    public void updateBounds(double upperBound, double lowerBound){
+        this.upperBound = upperBound;
+        this.lowerBound = lowerBound;
+    }
+
+    @Override
+    public int compareTo(MARKStarNode other){
+        /**
+         * Compares to another MARKStarNode.
+         * The node with a larger difference between upper and lower bound is less.
+         *
+         * @param   other the {@code MARKStarNode} to compare
+         * @return  1 if the other difference is smaller, -1 if the other difference
+         *          is larger, and 0 otherwise.
+         */
+        if ((this.upperBound - this.lowerBound) < (other.upperBound - other.lowerBound)){
+            return 1;
+        }else if ((this.upperBound - this.lowerBound) > (other.upperBound - other.lowerBound)){
+            return -1;
+        }else {
+            return 0;
+        }
+    }
+
+
+
 }
