cb4765e8a9a9e012f85bbe39277c1df4ed670854
Jonathan Jou <jj@cs.duke.edu>
2018-07-10 22:38:14 -0400

    Transfer home, also commit jvm fix.

--- src/edu/duke/cs/osprey/markstar/framework/MARKStarBound.java
+++ src/edu/duke/cs/osprey/markstar/framework/MARKStarBound.java
@@ -647,7 +647,7 @@ public class MARKStarBound implements PartitionFunction {
         Collections.sort(sortedPairwiseTerms, (a,b)->-Double.compare(a.getValue(),b.getValue()));
 
         //Collections.sort(sortedPairwiseTerms, Comparator.comparingDouble(Pair::getValue));
-        double threshhold = 1;
+        double threshhold = 0.3;
         for(int i = 0; i < sortedPairwiseTerms.size(); i++)
         {
             Pair<Pair<Integer, Integer>, Double> pairEnergy = sortedPairwiseTerms.get(i);
