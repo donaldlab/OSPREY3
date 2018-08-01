74770b2ab2b0cb2d7977d5bc43a98dfbc9c3b82b
Graham Holt <graham.t.holt@gmail.com>
2018-08-01 14:30:57 -0400

    slight modification of partial min reporting

--- src/edu/duke/cs/osprey/kstar/pfunc/GradientDescentPfunc.java
+++ src/edu/duke/cs/osprey/kstar/pfunc/GradientDescentPfunc.java
@@ -608,8 +608,8 @@ public class GradientDescentPfunc implements PartitionFunction.WithConfTable, Pa
 	}
 	@Override
 	public PartitionFunction.Result makeResult() {
-		ArrayList<Integer> minList = new ArrayList(Collections.nCopies(rcs.getNumPos(), 0));
-		minList.set(rcs.getNumPos(), getNumConfsEvaluated());
+		ArrayList<Integer> minList = new ArrayList(Collections.nCopies(rcs.getNumPos()-1, 0));
+		minList.set(rcs.getNumPos()-1, getNumConfsEvaluated());
 		return new PartitionFunction.Result(getStatus(), getValues(), getNumConfsEvaluated(), 0,getNumConfsScored(), energyConfs.getNumConformations(),Long.toString(stopwatch.getTimeNs()), minList);
 	}
 }
