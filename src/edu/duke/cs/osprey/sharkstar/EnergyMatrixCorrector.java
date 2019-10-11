4782ecf5516fbb2a17b3c33fc4454a66e164277a
Jonathan Jou <jj@cs.duke.edu>
2019-10-11 15:53:43 -0400

    Filter out energy matrix corrections.

---
+++ src/edu/duke/cs/osprey/sharkstar/EnergyMatrixCorrector.java
@@ -0,0 +1,184 @@
+package edu.duke.cs.osprey.sharkstar;
+
+import edu.duke.cs.osprey.astar.conf.RCs;
+import edu.duke.cs.osprey.confspace.ConfSearch;
+import edu.duke.cs.osprey.confspace.RCTuple;
+import edu.duke.cs.osprey.confspace.TupE;
+import edu.duke.cs.osprey.ematrix.EnergyMatrix;
+import edu.duke.cs.osprey.energy.ConfEnergyCalculator;
+import edu.duke.cs.osprey.energy.ResidueForcefieldBreakdown;
+import edu.duke.cs.osprey.gmec.ConfAnalyzer;
+import edu.duke.cs.osprey.tools.Stopwatch;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class EnergyMatrixCorrector {
+    private final MultiSequenceSHARKStarBound multiSequenceSHARKStarBound;
+
+    public EnergyMatrixCorrector(MultiSequenceSHARKStarBound multiSequenceSHARKStarBound) {
+        this.multiSequenceSHARKStarBound = multiSequenceSHARKStarBound;
+    }
+
+    void computeEnergyCorrection(ConfAnalyzer.ConfAnalysis analysis, ConfSearch.ScoredConf conf,
+                                 ConfEnergyCalculator ecalc, double epsilonBound) {
+        if (conf.getAssignments().length < 3)
+            return;
+        //System.out.println("Analysis:"+analysis);
+        EnergyMatrix energyAnalysis = analysis.breakdownEnergyByPosition(ResidueForcefieldBreakdown.Type.All);
+        EnergyMatrix scoreAnalysis = analysis.breakdownScoreByPosition(multiSequenceSHARKStarBound.getMinimizingEmat());
+        Stopwatch correctionTime = new Stopwatch().start();
+        //System.out.println("Energy Analysis: "+energyAnalysis);
+        //System.out.println("Score Analysis: "+scoreAnalysis);
+        EnergyMatrix diff = energyAnalysis.diff(scoreAnalysis);
+        //System.out.println("Difference Analysis " + diff);
+        List<TupE> sortedPairwiseTerms2 = new ArrayList<TupE>();
+        for (int pos = 0; pos < diff.getNumPos(); pos++) {
+            for (int rc = 0; rc < diff.getNumConfAtPos(pos); rc++) {
+                for (int pos2 = 0; pos2 < diff.getNumPos(); pos2++) {
+                    for (int rc2 = 0; rc2 < diff.getNumConfAtPos(pos2); rc2++) {
+                        if (pos >= pos2)
+                            continue;
+                        double sum = 0;
+                        sum += diff.getOneBody(pos, rc);
+                        sum += diff.getPairwise(pos, rc, pos2, rc2);
+                        sum += diff.getOneBody(pos2, rc2);
+                        TupE tupe = new TupE(new RCTuple(pos, rc, pos2, rc2), sum);
+                        sortedPairwiseTerms2.add(tupe);
+                    }
+                }
+            }
+        }
+        Collections.sort(sortedPairwiseTerms2);
+
+        double threshhold = 0.1;
+        double minDifference = 0.9;
+        double triplethreshhold = 0.3;
+        double maxDiff = sortedPairwiseTerms2.get(0).E;
+        for (int i = 0; i < sortedPairwiseTerms2.size(); i++) {
+            TupE tupe = sortedPairwiseTerms2.get(i);
+            double pairDiff = tupe.E;
+            if (pairDiff < minDifference && maxDiff - pairDiff > threshhold)
+                continue;
+            maxDiff = Math.max(maxDiff, tupe.E);
+            int pos1 = tupe.tup.pos.get(0);
+            int pos2 = tupe.tup.pos.get(1);
+            int localMinimizations = 0;
+            for (int pos3 = 0; pos3 < diff.getNumPos(); pos3++) {
+                if (pos3 == pos2 || pos3 == pos1)
+                    continue;
+                RCTuple tuple = makeTuple(conf, pos1, pos2, pos3);
+                double tupleBounds = multiSequenceSHARKStarBound.getRigidEmat().getInternalEnergy(tuple) - multiSequenceSHARKStarBound.getMinimizingEmat().getInternalEnergy(tuple);
+                if (tupleBounds < triplethreshhold)
+                    continue;
+                multiSequenceSHARKStarBound.minList.set(tuple.size() - 1, multiSequenceSHARKStarBound.minList.get(tuple.size() - 1) + 1);
+                computeDifference(tuple, multiSequenceSHARKStarBound.getMinimizingEcalc());
+                localMinimizations++;
+            }
+            multiSequenceSHARKStarBound.setNumPartialMinimizations(multiSequenceSHARKStarBound.getNumPartialMinimizations() + localMinimizations);
+            multiSequenceSHARKStarBound.getProgress().reportPartialMinimization(localMinimizations, epsilonBound);
+        }
+        correctionTime.stop();
+        ecalc.tasks.waitForFinish();
+    }
+
+    void computeDifference(RCTuple tuple, ConfEnergyCalculator minimizingEcalc) {
+        multiSequenceSHARKStarBound.setComputedCorrections(true);
+        if (multiSequenceSHARKStarBound.getCorrectedTuples().contains(tuple.stringListing()))
+            return;
+        multiSequenceSHARKStarBound.getCorrectedTuples().add(tuple.stringListing());
+        if (multiSequenceSHARKStarBound.getCorrectionMatrix().hasHigherOrderTermFor(tuple))
+            return;
+        minimizingEcalc.calcEnergyAsync(tuple, (minimizedTuple) -> {
+            double tripleEnergy = minimizedTuple.energy;
+
+            double lowerbound = multiSequenceSHARKStarBound.getMinimizingEmat().getInternalEnergy(tuple);
+            if (tripleEnergy - lowerbound > 0) {
+                double correction = tripleEnergy - lowerbound;
+                multiSequenceSHARKStarBound.getCorrectionMatrix().setHigherOrder(tuple, correction);
+            } else
+                System.err.println("Negative correction for " + tuple.stringListing());
+        });
+    }
+
+    RCTuple makeTuple(ConfSearch.ScoredConf conf, int... positions) {
+        RCTuple out = new RCTuple();
+        for (int pos : positions)
+            out = out.addRC(pos, conf.getAssignments()[pos]);
+        return out;
+    }
+
+    void processPreminimization(SingleSequenceSHARKStarBound bound, ConfEnergyCalculator ecalc) {
+        PriorityQueue<MultiSequenceSHARKStarNode> queue = bound.fringeNodes;
+        RCs RCs = bound.seqRCs;
+        int maxMinimizations = 1;//parallelism.numThreads;
+        List<MultiSequenceSHARKStarNode> topConfs = getTopConfs(queue, maxMinimizations);
+        // Need at least two confs to do any partial preminimization
+        if (topConfs.size() < 2) {
+            queue.addAll(topConfs);
+            return;
+        }
+        RCTuple lowestBoundTuple = topConfs.get(0).toTuple();
+        RCTuple overlap = findLargestOverlap(lowestBoundTuple, topConfs, 3);
+        //Only continue if we have something to minimize
+        for (MultiSequenceSHARKStarNode conf : topConfs) {
+            RCTuple confTuple = conf.toTuple();
+            if (multiSequenceSHARKStarBound.getMinimizingEmat().getInternalEnergy(confTuple) == multiSequenceSHARKStarBound.getRigidEmat().getInternalEnergy(confTuple))
+                continue;
+            multiSequenceSHARKStarBound.setNumPartialMinimizations(multiSequenceSHARKStarBound.getNumPartialMinimizations() + 1);
+            multiSequenceSHARKStarBound.minList.set(confTuple.size() - 1, multiSequenceSHARKStarBound.minList.get(confTuple.size() - 1) + 1);
+            if (confTuple.size() > 2 && confTuple.size() < RCs.getNumPos()) {
+                multiSequenceSHARKStarBound.getMinimizingEcalc().tasks.submit(() -> {
+                    computeTupleCorrection(multiSequenceSHARKStarBound.getMinimizingEcalc(), conf.toTuple(), bound.getSequenceEpsilon());
+                    return null;
+                }, (econf) -> {
+                });
+            }
+        }
+        //minimizingEcalc.tasks.waitForFinish();
+        if (overlap.size() > 3 && !multiSequenceSHARKStarBound.getCorrectionMatrix().hasHigherOrderTermFor(overlap)
+                && multiSequenceSHARKStarBound.getMinimizingEmat().getInternalEnergy(overlap) != multiSequenceSHARKStarBound.getRigidEmat().getInternalEnergy(overlap)) {
+            multiSequenceSHARKStarBound.getMinimizingEcalc().tasks.submit(() -> {
+                computeTupleCorrection(ecalc, overlap, bound.getSequenceEpsilon());
+                return null;
+            }, (econf) -> {
+            });
+        }
+        queue.addAll(topConfs);
+    }
+
+    void computeTupleCorrection(ConfEnergyCalculator ecalc, RCTuple overlap, double epsilonBound) {
+        if (multiSequenceSHARKStarBound.getCorrectionMatrix().hasHigherOrderTermFor(overlap))
+            return;
+        double pairwiseLower = multiSequenceSHARKStarBound.getMinimizingEmat().getInternalEnergy(overlap);
+        double partiallyMinimizedLower = ecalc.calcEnergy(overlap).energy;
+        multiSequenceSHARKStarBound.getProgress().reportPartialMinimization(1, epsilonBound);
+        if (partiallyMinimizedLower > pairwiseLower)
+            synchronized (multiSequenceSHARKStarBound.getCorrectionMatrix()) {
+                multiSequenceSHARKStarBound.getCorrectionMatrix().setHigherOrder(overlap, partiallyMinimizedLower - pairwiseLower);
+            }
+        multiSequenceSHARKStarBound.getProgress().reportPartialMinimization(1, epsilonBound);
+    }
+
+    List<MultiSequenceSHARKStarNode> getTopConfs(PriorityQueue<MultiSequenceSHARKStarNode> queue, int numConfs) {
+        List<MultiSequenceSHARKStarNode> topConfs = new ArrayList<MultiSequenceSHARKStarNode>();
+        while (topConfs.size() < numConfs && !queue.isEmpty()) {
+            MultiSequenceSHARKStarNode nextLowestConf = queue.poll();
+            topConfs.add(nextLowestConf);
+        }
+        return topConfs;
+    }
+
+    RCTuple findLargestOverlap(RCTuple conf, List<MultiSequenceSHARKStarNode> otherConfs, int minResidues) {
+        RCTuple overlap = conf;
+        for (MultiSequenceSHARKStarNode other : otherConfs) {
+            overlap = overlap.intersect(other.toTuple());
+            if (overlap.size() < minResidues)
+                break;
+        }
+        return overlap;
+
+    }
+}
--- src/edu/duke/cs/osprey/sharkstar/MultiSequenceSHARKStarBound.java
+++ src/edu/duke/cs/osprey/sharkstar/MultiSequenceSHARKStarBound.java
@@ -12,7 +12,6 @@ import edu.duke.cs.osprey.confspace.SimpleConfSpace;
 import edu.duke.cs.osprey.ematrix.EnergyMatrix;
 import edu.duke.cs.osprey.ematrix.UpdatingEnergyMatrix;
 import edu.duke.cs.osprey.energy.ConfEnergyCalculator;
-import edu.duke.cs.osprey.energy.ResidueForcefieldBreakdown;
 import edu.duke.cs.osprey.gmec.ConfAnalyzer;
 import edu.duke.cs.osprey.kstar.pfunc.BoltzmannCalculator;
 import edu.duke.cs.osprey.kstar.pfunc.PartitionFunction;
@@ -37,6 +36,7 @@ import static edu.duke.cs.osprey.sharkstar.tools.MultiSequenceSHARKStarNodeStati
 
 public class MultiSequenceSHARKStarBound implements PartitionFunction {
 
+    private final EnergyMatrixCorrector energyMatrixCorrector = new EnergyMatrixCorrector(this);
     private Sequence precomputedSequence;
     protected double targetEpsilon = 1;
     public static final boolean debug = true;
@@ -780,7 +780,7 @@ public class MultiSequenceSHARKStarBound implements PartitionFunction {
         profilePrint("Processed " + numNodes + " this loop, spawning " + newNodes.size() + " in " + loopTime + ", " + stopwatch.getTime() + " so far");
         loopWatch.reset();
         loopWatch.start();
-        processPreminimization(bound, minimizingEcalc);
+        energyMatrixCorrector.processPreminimization(bound, minimizingEcalc);
         profilePrint("Preminimization time : " + loopWatch.getTime(2));
         double curEpsilon = bound.getSequenceEpsilon();
         //printTree(bound.sequence, rootNode);
@@ -994,6 +994,46 @@ public class MultiSequenceSHARKStarBound implements PartitionFunction {
                 .collect(Collectors.toList());
     }
 
+    public int getNumPartialMinimizations() {
+        return numPartialMinimizations;
+    }
+
+    public MARKStarProgress getProgress() {
+        return progress;
+    }
+
+    public EnergyMatrix getRigidEmat() {
+        return rigidEmat;
+    }
+
+    public boolean isComputedCorrections() {
+        return computedCorrections;
+    }
+
+    public EnergyMatrix getMinimizingEmat() {
+        return minimizingEmat;
+    }
+
+    public ConfEnergyCalculator getMinimizingEcalc() {
+        return minimizingEcalc;
+    }
+
+    public Set<String> getCorrectedTuples() {
+        return correctedTuples;
+    }
+
+    public UpdatingEnergyMatrix getCorrectionMatrix() {
+        return correctionMatrix;
+    }
+
+    public void setNumPartialMinimizations(int numPartialMinimizations) {
+        this.numPartialMinimizations = numPartialMinimizations;
+    }
+
+    public void setComputedCorrections(boolean computedCorrections) {
+        this.computedCorrections = computedCorrections;
+    }
+
     private class PartialConfNodeResult {
         Node resultNode = null;
         double upperBound = Double.NaN;
@@ -1165,7 +1205,7 @@ public class MultiSequenceSHARKStarBound implements PartitionFunction {
                         ConfSearch.ScoredConf conf = new ConfSearch.ScoredConf(node.assignments, curNode.getConfLowerBound(bound.sequence));
                         ConfAnalyzer.ConfAnalysis analysis = confAnalyzer.analyze(conf);
                         Stopwatch correctionTimer = new Stopwatch().start();
-                        computeEnergyCorrection(analysis, conf, context.ecalc, bound.getSequenceEpsilon());
+                        energyMatrixCorrector.computeEnergyCorrection(analysis, conf, context.ecalc, bound.getSequenceEpsilon());
 
                         double energy = analysis.epmol.energy;
                         double newConfUpper = energy;
@@ -1227,168 +1267,6 @@ public class MultiSequenceSHARKStarBound implements PartitionFunction {
     private void checkBounds(double lower, double upper) {
         if (upper < lower && upper - lower > 1e-5 && upper < 10)
             debugPrint("Bounds incorrect.");
-    }
-
-    private void computeEnergyCorrection(ConfAnalyzer.ConfAnalysis analysis, ConfSearch.ScoredConf conf,
-                                         ConfEnergyCalculator ecalc, double epsilonBound) {
-        if (conf.getAssignments().length < 3)
-            return;
-        //System.out.println("Analysis:"+analysis);
-        EnergyMatrix energyAnalysis = analysis.breakdownEnergyByPosition(ResidueForcefieldBreakdown.Type.All);
-        EnergyMatrix scoreAnalysis = analysis.breakdownScoreByPosition(minimizingEmat);
-        Stopwatch correctionTime = new Stopwatch().start();
-        //System.out.println("Energy Analysis: "+energyAnalysis);
-        //System.out.println("Score Analysis: "+scoreAnalysis);
-        EnergyMatrix diff = energyAnalysis.diff(scoreAnalysis);
-        //System.out.println("Difference Analysis " + diff);
-        List<TupE> sortedPairwiseTerms2 = new ArrayList<>();
-        for (int pos = 0; pos < diff.getNumPos(); pos++) {
-            for (int rc = 0; rc < diff.getNumConfAtPos(pos); rc++) {
-                for (int pos2 = 0; pos2 < diff.getNumPos(); pos2++) {
-                    for (int rc2 = 0; rc2 < diff.getNumConfAtPos(pos2); rc2++) {
-                        if (pos >= pos2)
-                            continue;
-                        double sum = 0;
-                        sum += diff.getOneBody(pos, rc);
-                        sum += diff.getPairwise(pos, rc, pos2, rc2);
-                        sum += diff.getOneBody(pos2, rc2);
-                        TupE tupe = new TupE(new RCTuple(pos, rc, pos2, rc2), sum);
-                        sortedPairwiseTerms2.add(tupe);
-                    }
-                }
-            }
-        }
-        Collections.sort(sortedPairwiseTerms2);
-
-        double threshhold = 0.1;
-        double minDifference = 0.9;
-        double triplethreshhold = 0.3;
-        double maxDiff = sortedPairwiseTerms2.get(0).E;
-        for (int i = 0; i < sortedPairwiseTerms2.size(); i++) {
-            TupE tupe = sortedPairwiseTerms2.get(i);
-            double pairDiff = tupe.E;
-            if (pairDiff < minDifference && maxDiff - pairDiff > threshhold)
-                continue;
-            maxDiff = Math.max(maxDiff, tupe.E);
-            int pos1 = tupe.tup.pos.get(0);
-            int pos2 = tupe.tup.pos.get(1);
-            int localMinimizations = 0;
-            for (int pos3 = 0; pos3 < diff.getNumPos(); pos3++) {
-                if (pos3 == pos2 || pos3 == pos1)
-                    continue;
-                RCTuple tuple = makeTuple(conf, pos1, pos2, pos3);
-                double tupleBounds = rigidEmat.getInternalEnergy(tuple) - minimizingEmat.getInternalEnergy(tuple);
-                if (tupleBounds < triplethreshhold)
-                    continue;
-                minList.set(tuple.size() - 1, minList.get(tuple.size() - 1) + 1);
-                computeDifference(tuple, minimizingEcalc);
-                localMinimizations++;
-            }
-            numPartialMinimizations += localMinimizations;
-            progress.reportPartialMinimization(localMinimizations, epsilonBound);
-        }
-        correctionTime.stop();
-        ecalc.tasks.waitForFinish();
-    }
-
-
-    private void computeDifference(RCTuple tuple, ConfEnergyCalculator minimizingEcalc) {
-        computedCorrections = true;
-        if (correctedTuples.contains(tuple.stringListing()))
-            return;
-        correctedTuples.add(tuple.stringListing());
-        if (correctionMatrix.hasHigherOrderTermFor(tuple))
-            return;
-        minimizingEcalc.calcEnergyAsync(tuple, (minimizedTuple) -> {
-            double tripleEnergy = minimizedTuple.energy;
-
-            double lowerbound = minimizingEmat.getInternalEnergy(tuple);
-            if (tripleEnergy - lowerbound > 0) {
-                double correction = tripleEnergy - lowerbound;
-                correctionMatrix.setHigherOrder(tuple, correction);
-            } else
-                System.err.println("Negative correction for " + tuple.stringListing());
-        });
-    }
-
-    private RCTuple makeTuple(ConfSearch.ScoredConf conf, int... positions) {
-        RCTuple out = new RCTuple();
-        for (int pos : positions)
-            out = out.addRC(pos, conf.getAssignments()[pos]);
-        return out;
-    }
-
-    private void processPreminimization(SingleSequenceSHARKStarBound bound, ConfEnergyCalculator ecalc) {
-        PriorityQueue<MultiSequenceSHARKStarNode> queue = bound.fringeNodes;
-        RCs RCs = bound.seqRCs;
-        int maxMinimizations = 1;//parallelism.numThreads;
-        List<MultiSequenceSHARKStarNode> topConfs = getTopConfs(queue, maxMinimizations);
-        // Need at least two confs to do any partial preminimization
-        if (topConfs.size() < 2) {
-            queue.addAll(topConfs);
-            return;
-        }
-        RCTuple lowestBoundTuple = topConfs.get(0).toTuple();
-        RCTuple overlap = findLargestOverlap(lowestBoundTuple, topConfs, 3);
-        //Only continue if we have something to minimize
-        for (MultiSequenceSHARKStarNode conf : topConfs) {
-            RCTuple confTuple = conf.toTuple();
-            if (minimizingEmat.getInternalEnergy(confTuple) == rigidEmat.getInternalEnergy(confTuple))
-                continue;
-            numPartialMinimizations++;
-            minList.set(confTuple.size() - 1, minList.get(confTuple.size() - 1) + 1);
-            if (confTuple.size() > 2 && confTuple.size() < RCs.getNumPos()) {
-                minimizingEcalc.tasks.submit(() -> {
-                    computeTupleCorrection(minimizingEcalc, conf.toTuple(), bound.getSequenceEpsilon());
-                    return null;
-                }, (econf) -> {
-                });
-            }
-        }
-        //minimizingEcalc.tasks.waitForFinish();
-        if (overlap.size() > 3 && !correctionMatrix.hasHigherOrderTermFor(overlap)
-                && minimizingEmat.getInternalEnergy(overlap) != rigidEmat.getInternalEnergy(overlap)) {
-            minimizingEcalc.tasks.submit(() -> {
-                computeTupleCorrection(ecalc, overlap, bound.getSequenceEpsilon());
-                return null;
-            }, (econf) -> {
-            });
-        }
-        queue.addAll(topConfs);
-    }
-
-    private void computeTupleCorrection(ConfEnergyCalculator ecalc, RCTuple overlap, double epsilonBound) {
-        if (correctionMatrix.hasHigherOrderTermFor(overlap))
-            return;
-        double pairwiseLower = minimizingEmat.getInternalEnergy(overlap);
-        double partiallyMinimizedLower = ecalc.calcEnergy(overlap).energy;
-        progress.reportPartialMinimization(1, epsilonBound);
-        if (partiallyMinimizedLower > pairwiseLower)
-            synchronized (correctionMatrix) {
-                correctionMatrix.setHigherOrder(overlap, partiallyMinimizedLower - pairwiseLower);
-            }
-        progress.reportPartialMinimization(1, epsilonBound);
-    }
-
-    private List<MultiSequenceSHARKStarNode> getTopConfs(PriorityQueue<MultiSequenceSHARKStarNode> queue, int numConfs) {
-        List<MultiSequenceSHARKStarNode> topConfs = new ArrayList<>();
-        while (topConfs.size() < numConfs && !queue.isEmpty()) {
-            MultiSequenceSHARKStarNode nextLowestConf = queue.poll();
-            topConfs.add(nextLowestConf);
-        }
-        return topConfs;
-    }
-
-
-    private RCTuple findLargestOverlap(RCTuple conf, List<MultiSequenceSHARKStarNode> otherConfs, int minResidues) {
-        RCTuple overlap = conf;
-        for (MultiSequenceSHARKStarNode other : otherConfs) {
-            overlap = overlap.intersect(other.toTuple());
-            if (overlap.size() < minResidues)
-                break;
-        }
-        return overlap;
-
     }
 
     private boolean hasPrunedPair(ConfIndex confIndex, int nextPos, int nextRc) {
