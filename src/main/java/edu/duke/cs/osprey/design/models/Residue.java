8f04326ccb8aa60e65ccad4fe3f3381e33f0402f
Nate Guerin <nate@textpla.in>
2019-10-31 16:38:28 -0400

    Ignore unknown properties on Residue class

--- src/main/java/edu/duke/cs/osprey/design/models/Residue.java
+++ src/main/java/edu/duke/cs/osprey/design/models/Residue.java
@@ -1,7 +1,9 @@
 package edu.duke.cs.osprey.design.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Residue {
     public String chain;
     @JsonProperty("res_num")
