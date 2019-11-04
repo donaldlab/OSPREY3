4e807ee1e2e58e0ef607207f00d7cf40b52c06a5
Nate Guerin <nate@textpla.in>
2019-11-04 13:13:48 -0500

    Incorporate bounds on entropy and enthalpy

--- src/main/java/edu/duke/cs/osprey/design/analysis/ThermodynamicsConfListener.java
+++ src/main/java/edu/duke/cs/osprey/design/analysis/ThermodynamicsConfListener.java
@@ -14,9 +14,10 @@ import java.util.List;
 public class ThermodynamicsConfListener implements CommandAnalysis {
 
     private List<ConfSearch.EnergiedConf> confs = new ArrayList<>();
-    private BigDecimal pFuncValue;
+    private BigDecimal pFuncLowerBound;
+    private BigDecimal pFuncUpperBound;
     private ExpFunction eFn = new ExpFunction();
-    private BoltzmannCalculator bCalc = new BoltzmannCalculator(PartitionFunction.decimalPrecision);
+    private static BoltzmannCalculator bCalc = new BoltzmannCalculator(PartitionFunction.decimalPrecision);
 
     @Override
     public void onConf(ConfSearch.ScoredConf conf) {
@@ -25,41 +26,67 @@ public class ThermodynamicsConfListener implements CommandAnalysis {
 
     @Override
     public void finished(PartitionFunction pfunc) {
-        pFuncValue = pfunc.getValues().qstar;
+        var values = pfunc.getValues();
+        pFuncUpperBound = values.calcUpperBound();
+        pFuncLowerBound = pfunc.getValues().calcLowerBound();
     }
 
     private static BigMath makeBigMath() {
         return new BigMath(PartitionFunction.decimalPrecision);
     }
 
-
-    private BigDecimal getProbability(ConfSearch.EnergiedConf conf) {
+    private static BigDecimal getBoundedProbability(ConfSearch.EnergiedConf conf, BigDecimal bound) {
         return makeBigMath()
                 .set(bCalc.calc(conf.getEnergy()))
-                .div(pFuncValue)
+                .div(bound)
                 .get();
     }
 
-    public BigDecimal getEnthalpy() {
-        var math = makeBigMath();
+    private BigDecimal getUpperBoundProbability(ConfSearch.EnergiedConf conf) {
+        return getBoundedProbability(conf, pFuncLowerBound);
+    }
 
+    private BigDecimal getLowerBoundProbability(ConfSearch.EnergiedConf conf) {
+        return getBoundedProbability(conf, pFuncUpperBound);
+    }
+
+    public BigDecimal getUpperBoundEnthalpy() {
+        var math = makeBigMath();
         return confs.stream()
-                .map(conf -> math.set(getProbability(conf)).mult(conf.getEnergy()).get())
+                .map(conf -> math.set(getLowerBoundProbability(conf)).mult(conf.getEnergy()).get())
                 .reduce(BigDecimal::add)
                 .orElseThrow();
     }
 
-    public BigDecimal getEntropy() {
-        var math = makeBigMath();
+    public BigDecimal getLowerBoundEnthalpy() {
+        var upperBoundProbabilitySum = confs.stream()
+                .map(this::getUpperBoundProbability)
+                .reduce(BigDecimal::add)
+                .orElseThrow();
 
-        return math.set(Constants.R)
-                .mult(eFn.log(pFuncValue))
-                .add(getEnthalpy().doubleValue() / Constants.T)
+        var restOfProbability = makeBigMath().set(1).sub(upperBoundProbabilitySum);
+        var restOfProbabilityTimesWorstEnergyConf = restOfProbability.mult(confs.get(confs.size() - 1).getEnergy()).get();
+
+        return upperBoundProbabilitySum.add(restOfProbabilityTimesWorstEnergyConf);
+    }
+
+    public BigDecimal getUpperBoundEntropy() {
+        return makeBigMath().set(Constants.R)
+                .mult(eFn.log(pFuncUpperBound))
+                .add(getUpperBoundEnthalpy().doubleValue() / Constants.T)
+                .get();
+    }
+
+    public BigDecimal getLowerBoundEntropy() {
+        return makeBigMath().set(Constants.R)
+                .mult(eFn.log(pFuncLowerBound))
+                .add(getLowerBoundEnthalpy().doubleValue() / Constants.T)
                 .get();
     }
 
     @Override
     public void printResults() {
-        System.out.println(String.format("Enthalpy: %.04f, Entropy: %.04f", getEnthalpy(), getEntropy()));
+        System.out.println(String.format("Enthalpy[%.04f - %.04f]", getLowerBoundEnthalpy(), getUpperBoundEnthalpy()));
+        System.out.println(String.format("Entropy[%.04f - %.04f]", getLowerBoundEntropy(), getUpperBoundEntropy()));
     }
 }
