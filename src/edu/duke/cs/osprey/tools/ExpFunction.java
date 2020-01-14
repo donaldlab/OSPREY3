ff83db7c3b79f61423908b179a544d3dd6a64b87
Graham <graham.t.holt@gmail.com>
2020-01-14 14:24:22 -0500

    Added the logStable function, which does not currently work well, as well as tests of the current log function

--- src/edu/duke/cs/osprey/tools/ExpFunction.java
+++ src/edu/duke/cs/osprey/tools/ExpFunction.java
@@ -51,6 +51,7 @@ package edu.duke.cs.osprey.tools;
 
 import edu.duke.cs.osprey.kstar.pfunc.PartitionFunction;
 
+import javax.annotation.PreDestroy;
 import java.io.Serializable;
 import java.math.*;
 
@@ -115,8 +116,57 @@ public class ExpFunction implements Serializable {
 		return num.pow(a);
 	}
 
+	@Deprecated
+	public BigDecimal logStable(BigDecimal num){
+		/**logStable
+		 *
+		 * Computes an approximation to the natural logarithm of BigDecimal number num.
+		 * Conceptually the same as the log method, but we count up to avoid scale issues
+		 * that were happening with the other method.
+         *
+		 * NOTE: THIS DOESN'T CURRENTLY WORK AS ACCURATELY AS THE OTHER LOG FUNCTION
+		 */
+		if (num.compareTo(new BigDecimal("0.0"))<0){ //num is negative
+			throw new IllegalArgumentException("log of a negative number: " + num);
+		}
+
+		BigDecimal sum = new BigDecimal("0.0");
+		BigDecimal x = BigDecimal.ONE;
+
+		if (num.compareTo(cutoffLog)<0){ //num is small, so use the standard Math.log() function
+			if (num.compareTo(new BigDecimal(1e-323))<0)
+				sum = MathTools.BigNegativeInfinity;
+			else
+				sum = new BigDecimal(Math.log(num.doubleValue()));
+		}
+		else { //num is large, so compute an approximation to the natural logarithm
+
+			double t = 0.0;
+
+			boolean done = false;
+			while (!done){
+				if (x.compareTo(num)<0){
+					t += 1.0;
+					x = x.multiply(exp, mc);
+				}
+				else {
+					double addend = Math.log(num.subtract(x,mc).doubleValue());
+					sum = sum.add(new BigDecimal(t));
+					if(Double.isFinite(addend))
+						sum = sum.add(new BigDecimal(addend));
+					done = true;
+				}
+			}
+		}
+
+		return sum;
+
+	}
 	//Returns an approximation to the natural logarithm of the BigDecimal number num
 	public BigDecimal log(BigDecimal num){
+		/**
+		 * Note: This starts to fail if num's exponent is much bigger than the MathContext precision + 36, empirically
+		 */
 		if (num.compareTo(new BigDecimal("0.0"))<0){ //num is negative
 			throw new IllegalArgumentException("log of a negative number: " + num);
 		}
--- test/edu/duke/cs/osprey/tools/TestExpFunction.java
+++ test/edu/duke/cs/osprey/tools/TestExpFunction.java
@@ -33,6 +33,11 @@
 package edu.duke.cs.osprey.tools;
 
 import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 import static org.junit.Assert.*;
 import static edu.duke.cs.osprey.TestBase.*;
@@ -56,5 +61,84 @@ public class TestExpFunction {
 		assertThat(exp.exp(  5.0).doubleValue(), isAbsolutely(Math.exp(  5.0), Epsilon));
 		assertThat(exp.exp(  7.5).doubleValue(), isAbsolutely(Math.exp(  7.5), Epsilon));
 		assertThat(exp.exp( 10.0).doubleValue(), isAbsolutely(Math.exp( 10.0), Epsilon));
+	}
+
+	@Test
+	public void testLog(){
+		final double Epsilon = 1e-5;
+		final MathContext smallest = new MathContext(8, RoundingMode.HALF_UP);
+		final MathContext middle = new MathContext(64, RoundingMode.HALF_UP);
+		final MathContext ourDefault = new MathContext(100, RoundingMode.HALF_UP);
+
+		ExpFunction exp = new ExpFunction();
+		assertThat(exp.log(exp.exp(-10)).doubleValue(), isAbsolutely(-10.0, Epsilon));
+		assertThat(exp.log(exp.exp(-7.5)).doubleValue(), isAbsolutely(-7.5, Epsilon));
+		assertThat(exp.log(exp.exp(-5.0)).doubleValue(), isAbsolutely(-5.0, Epsilon));
+		assertThat(exp.log(exp.exp(-2.5)).doubleValue(), isAbsolutely(-2.5, Epsilon));
+		assertThat(exp.log(exp.exp(-1.0)).doubleValue(), isAbsolutely(-1.0, Epsilon));
+		assertThat(exp.log(exp.exp(-0.5)).doubleValue(), isAbsolutely(-0.5, Epsilon));
+		assertThat(exp.log(exp.exp(0.0)).doubleValue(), isAbsolutely(0.0, Epsilon));
+		assertThat(exp.log(exp.exp(0.5)).doubleValue(), isAbsolutely(0.5, Epsilon));
+		assertThat(exp.log(exp.exp(1.0)).doubleValue(), isAbsolutely(1.0, Epsilon));
+		assertThat(exp.log(exp.exp(2.5)).doubleValue(), isAbsolutely(2.5, Epsilon));
+		assertThat(exp.log(exp.exp(5.0)).doubleValue(), isAbsolutely(5.0, Epsilon));
+		assertThat(exp.log(exp.exp(7.5)).doubleValue(), isAbsolutely(7.5, Epsilon));
+		assertThat(exp.log(exp.exp(10.0)).doubleValue(), isAbsolutely(10.0, Epsilon));
+
+		// this log function will fail if the value's exponent is much greater than the precision + 36, empirically
+		// testing the effect of precision on the logs
+		assertNotEquals(exp.log(new BigDecimal(6.091e71, smallest)).doubleValue(), isAbsolutely(165.290353874, Epsilon)); // this one fails
+		assertThat(exp.log(new BigDecimal(6.091e71, middle)).doubleValue(), isAbsolutely(165.290353874, Epsilon));
+		assertThat(exp.log(new BigDecimal(6.091e71, ourDefault)).doubleValue(), isAbsolutely(165.290353874, Epsilon));
+
+		assertNotEquals(exp.log(new BigDecimal(1.245e111, smallest)).doubleValue(), isAbsolutely(255.883339335, Epsilon)); // this one fails
+		assertNotEquals(exp.log(new BigDecimal(1.245e111, middle)).doubleValue(), isAbsolutely(255.883339335, Epsilon)); // this one fails
+		assertThat(exp.log(new BigDecimal(1.245e111, ourDefault)).doubleValue(), isAbsolutely(255.883339335, 0.1)); // errors are starting to increase!
+		assertThat(exp.log(new BigDecimal(1.245e111, new MathContext(75, RoundingMode.HALF_UP))).doubleValue(), isAbsolutely(255.883339335, 0.1)); // errors are starting to increase!
+
+		assertNotEquals(exp.log(new BigDecimal(5.927e300, smallest)).doubleValue(), isAbsolutely(692.555046081, Epsilon)); // this one fails
+		assertNotEquals(exp.log(new BigDecimal(5.927e300, middle)).doubleValue(), isAbsolutely(692.555046081, Epsilon)); // this one fails
+		assertNotEquals(exp.log(new BigDecimal(5.927e300, ourDefault)).doubleValue(), isAbsolutely(692.555046081, Epsilon)); // this one fails
+		assertThat(exp.log(new BigDecimal(5.927e300, new MathContext(264, RoundingMode.HALF_UP))).doubleValue(), isAbsolutely(692.555046081, 0.1));
+		assertThat(exp.log(new BigDecimal(5.927e300, new MathContext(300, RoundingMode.HALF_UP))).doubleValue(), isAbsolutely(692.555046081, Epsilon));
+	}
+
+	@Test
+	public void testLogStable(){
+		final double Epsilon = 1e-5;
+		final MathContext smallest = new MathContext(8, RoundingMode.HALF_UP);
+		final MathContext middle = new MathContext(64, RoundingMode.HALF_UP);
+		final MathContext ourDefault = new MathContext(100, RoundingMode.HALF_UP);
+
+		ExpFunction exp = new ExpFunction();
+		assertThat(exp.logStable(exp.exp(-10)).doubleValue(), isAbsolutely(-10.0, Epsilon));
+		assertThat(exp.logStable(exp.exp(-7.5)).doubleValue(), isAbsolutely(-7.5, Epsilon));
+		assertThat(exp.logStable(exp.exp(-5.0)).doubleValue(), isAbsolutely(-5.0, Epsilon));
+		assertThat(exp.logStable(exp.exp(-2.5)).doubleValue(), isAbsolutely(-2.5, Epsilon));
+		assertThat(exp.logStable(exp.exp(-1.0)).doubleValue(), isAbsolutely(-1.0, Epsilon));
+		assertThat(exp.logStable(exp.exp(-0.5)).doubleValue(), isAbsolutely(-0.5, Epsilon));
+		assertThat(exp.logStable(exp.exp(0.0)).doubleValue(), isAbsolutely(0.0, Epsilon));
+		assertThat(exp.logStable(exp.exp(0.5)).doubleValue(), isAbsolutely(0.5, Epsilon));
+		assertThat(exp.logStable(exp.exp(1.0)).doubleValue(), isAbsolutely(1.0, Epsilon));
+		assertThat(exp.logStable(exp.exp(2.5)).doubleValue(), isAbsolutely(2.5, Epsilon));
+		assertThat(exp.logStable(exp.exp(5.0)).doubleValue(), isAbsolutely(5.0, Epsilon));
+		assertThat(exp.logStable(exp.exp(7.5)).doubleValue(), isAbsolutely(7.5, Epsilon));
+		assertThat(exp.logStable(exp.exp(10.0)).doubleValue(), isAbsolutely(10.0, Epsilon));
+
+		assertNotEquals(exp.logStable(new BigDecimal(6.091e71, smallest)).doubleValue(), isAbsolutely(165.290353874, Epsilon)); // this one fails
+		assertThat(exp.logStable(new BigDecimal(6.091e71, middle)).doubleValue(), isAbsolutely(165.290353874, Epsilon));
+		assertThat(exp.logStable(new BigDecimal(6.091e71, ourDefault)).doubleValue(), isAbsolutely(165.290353874, Epsilon));
+
+		assertNotEquals(exp.logStable(new BigDecimal(1.245e111, smallest)).doubleValue(), isAbsolutely(255.883339335, Epsilon)); // this one fails
+		assertNotEquals(exp.logStable(new BigDecimal(1.245e111, middle)).doubleValue(), isAbsolutely(255.883339335, Epsilon)); // this one fails
+		assertThat(exp.logStable(new BigDecimal(1.245e111, ourDefault)).doubleValue(), isAbsolutely(255.883339335, 0.1)); // errors are starting to increase!
+		assertThat(exp.logStable(new BigDecimal(1.245e111, new MathContext(75, RoundingMode.HALF_UP))).doubleValue(), isAbsolutely(255.883339335, 0.1)); // errors are starting to increase!
+
+		assertNotEquals(exp.logStable(new BigDecimal(5.927e300, smallest)).doubleValue(), isAbsolutely(692.555046081, Epsilon)); // this one fails
+		assertNotEquals(exp.logStable(new BigDecimal(5.927e300, middle)).doubleValue(), isAbsolutely(692.555046081, Epsilon)); // this one fails
+		assertNotEquals(exp.logStable(new BigDecimal(5.927e300, ourDefault)).doubleValue(), isAbsolutely(692.555046081, Epsilon)); // this one fails
+		assertThat(exp.logStable(new BigDecimal(5.927e300, new MathContext(264, RoundingMode.HALF_UP))).doubleValue(), isAbsolutely(692.555046081, 0.1));
+		assertThat(exp.logStable(new BigDecimal(5.927e300, new MathContext(300, RoundingMode.HALF_UP))).doubleValue(), isAbsolutely(692.555046081, Epsilon));
+
 	}
 }
