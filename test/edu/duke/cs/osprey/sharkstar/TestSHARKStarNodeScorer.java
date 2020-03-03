68ec64089f3912f529b534ed4f48d0b361ef1397
Graham <graham.t.holt@gmail.com>
2020-03-03 14:20:29 -0500

    Made it easier to run tests on multiple systems

--- test/edu/duke/cs/osprey/sharkstar/TestSHARKStarNodeScorer.java
+++ test/edu/duke/cs/osprey/sharkstar/TestSHARKStarNodeScorer.java
@@ -1,6 +1,5 @@
 package edu.duke.cs.osprey.sharkstar;
 
-import edu.duke.cs.osprey.astar.conf.ConfAStarNode;
 import edu.duke.cs.osprey.astar.conf.ConfIndex;
 import edu.duke.cs.osprey.astar.conf.RCs;
 import edu.duke.cs.osprey.astar.conf.scoring.AStarScorer;
@@ -14,17 +13,14 @@ import edu.duke.cs.osprey.energy.ConfEnergyCalculator;
 import edu.duke.cs.osprey.energy.EnergyCalculator;
 import edu.duke.cs.osprey.energy.forcefield.ForcefieldParams;
 import edu.duke.cs.osprey.kstar.pfunc.BoltzmannCalculator;
-import edu.duke.cs.osprey.markstar.framework.MARKStarNode;
 import edu.duke.cs.osprey.parallelism.Parallelism;
 import edu.duke.cs.osprey.tools.MathTools;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.ojalgo.matrix.transformation.Rotation;
 
 import java.io.FileNotFoundException;
 import java.math.BigDecimal;
 import java.math.MathContext;
-import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -35,73 +31,22 @@ import static edu.duke.cs.osprey.sharkstar.TestSHARKStar.loadFromCFS;
 
 public class TestSHARKStarNodeScorer {
 
-    private static String design = "test-resources/3bua_B_10res_4.363E+11.cfs";
-    private static MultiSequenceSHARKStarNode.Node rootNode;
-    private static RCs rcs;
-    private static int[] order = { 4, 5, 6, 0, 2, 1, 3};
-    private static List<MultiSequenceSHARKStarNode.Node> levelOne;
-    private static BoltzmannCalculator bc;
-    private static ConfIndex confIndex;
-
-    private static SHARKStarNodeScorer lbScorer;
-    private static SHARKStarNodeScorer ubScorer;
-    private static AStarScorer lbGScorer;
-    private static AStarScorer ubGScorer;
+    private static List<TestContext> systems;
 
     @BeforeClass
     public static void beforeClass() throws FileNotFoundException {
-        SimpleConfSpace confSpace = loadFromCFS(design).protein.makeFlexibleCopy();
-
-        ForcefieldParams ffparams = new ForcefieldParams();
-
-
-        // Define the minimizing energy calculator
-        EnergyCalculator minimizingEcalc = new EnergyCalculator.Builder(confSpace, ffparams)
-                .setParallelism(Parallelism.makeCpu(2))
-                .build();
-        // Define the rigid energy calculator
-        EnergyCalculator rigidEcalc = new EnergyCalculator.Builder(confSpace, ffparams)
-                .setParallelism(Parallelism.makeCpu(2))
-                .setIsMinimizing(false)
-                .build();
-        // Define the method to make confEnergyCalculators (with reference energies)
-        BBSHARKStar.ConfEnergyCalculatorFactory confEcalcFactory = (confSpaceArg, ecalcArg) -> {
-            return new ConfEnergyCalculator.Builder(confSpaceArg, ecalcArg)
-                    .setReferenceEnergies(new SimplerEnergyMatrixCalculator.Builder(confSpaceArg, minimizingEcalc)
-                            .build()
-                            .calcReferenceEnergies()
-                    )
-                    .build();
-        };
-        // Make the confEnergyCalculators
-        ConfEnergyCalculator rigidConfEcalc = confEcalcFactory.make(confSpace, rigidEcalc);
-        ConfEnergyCalculator minimizingConfEcalc = confEcalcFactory.make(confSpace, minimizingEcalc);
-
-        // Make the energy matrices
-        EnergyMatrix rigidEmat = new SimplerEnergyMatrixCalculator.Builder(rigidConfEcalc).build().calcEnergyMatrix();
-        EnergyMatrix minimizingEmat = new SimplerEnergyMatrixCalculator.Builder(minimizingConfEcalc).build().calcEnergyMatrix();
-        EnergyMatrix correctionEmat = new UpdatingEnergyMatrix(confSpace, minimizingEmat);
-
-        Sequence wildType = confSpace.makeWildTypeSequence();
-        rcs = wildType.makeRCs(confSpace);
-        rootNode = new MultiSequenceSHARKStarNode.Node(confSpace.positions.size(), 0, new MathTools.DoubleBounds());
-        levelOne = getLevelOne(rootNode);
-
-        bc = new BoltzmannCalculator(new MathContext(BigDecimal.ROUND_HALF_UP));
-
-        lbScorer = new SHARKStarNodeScorer(minimizingEmat, false);
-        ubScorer = new SHARKStarNodeScorer(rigidEmat, true);
-        lbGScorer = new PairwiseGScorer(minimizingEmat);
-        ubGScorer = new PairwiseGScorer(rigidEmat);
-
-        confIndex = new ConfIndex(rcs.getNumPos());
-        System.out.println("RCs:"+rcs.toString());
+        systems = new ArrayList<>();
+        systems.add(new TestContext("test-resources/3bua_B_10res_4.363E+11.cfs",
+                "protein",
+                new int[] { 4, 5, 6, 0, 2, 1, 3},
+                new HashMap<String,String>()
+        ));
     }
 
-    private static List<MultiSequenceSHARKStarNode.Node> getLevelOne(MultiSequenceSHARKStarNode.Node rootNode){
+    private static List<MultiSequenceSHARKStarNode.Node> getLevelOne(TestContext testContext, MultiSequenceSHARKStarNode.Node rootNode){
         List<MultiSequenceSHARKStarNode.Node> children = new ArrayList<>();
-        int nextPos = order[0];
-        for(int nextRC : rcs.get(nextPos)){
+        int nextPos = testContext.order[0];
+        for(int nextRC : testContext.rcs.get(nextPos)){
             MultiSequenceSHARKStarNode.Node child = rootNode.assign(nextPos, nextRC);
             children.add(child);
         }
@@ -110,94 +55,106 @@ public class TestSHARKStarNodeScorer {
 
     @Test
     public void testRootBound(){
-        rootNode.index(confIndex);
-        //int[] debugConf = {-1, -1, -1, -1, -1, -1, -1};
-        //lbScorer.setDebugConf(debugConf);
-        double lb = lbGScorer.calc(confIndex, rcs) + lbScorer.calc(confIndex,rcs);
-        double ub = ubGScorer.calc(confIndex, rcs) + ubScorer.calc(confIndex,rcs);
+        for (TestContext system : systems){
+            system.rootNode.index(system.confIndex);
+            //int[] debugConf = {-1, -1, -1, -1, -1, -1, -1};
+            //lbScorer.setDebugConf(debugConf);
+            double lb = system.lbGScorer.calc(system.confIndex, system.rcs) + system.lbScorer.calc(system.confIndex,system.rcs);
+            double ub = system.ubGScorer.calc(system.confIndex, system.rcs) + system.ubScorer.calc(system.confIndex,system.rcs);
 
-        System.out.println(String.format("%s bounds: [%.3e, %.3e] --> Energy [%.20f, %.20f]", rootNode.confToString(), bc.calc(ub), bc.calc(lb), lb, ub));
+            System.out.println(String.format("%s bounds: [%.3e, %.3e] --> Energy [%.20f, %.20f]", system.rootNode.confToString(), system.bc.calc(ub), system.bc.calc(lb), lb, ub));
+        }
     }
 
     @Test
     public void testRootBound_allatonce(){
-        rootNode.index(confIndex);
-        //int[] debugConf = {-1, -1, -1, -1, -1, -1, -1};
-        //lbScorer.setDebugConf(debugConf);
-        BigDecimal pfuncUpper = lbScorer.calcCombinedScore(confIndex,rcs);
-        BigDecimal pfuncLower =  ubScorer.calcCombinedScore(confIndex,rcs);
+        for (TestContext system : systems) {
+            system.rootNode.index(system.confIndex);
+            //int[] debugConf = {-1, -1, -1, -1, -1, -1, -1};
+            //lbScorer.setDebugConf(debugConf);
+            BigDecimal pfuncUpper = system.lbScorer.calcCombinedScore(system.confIndex, system.rcs);
+            BigDecimal pfuncLower = system.ubScorer.calcCombinedScore(system.confIndex, system.rcs);
 
-        System.out.println(String.format("%s bounds: [%.3e, %.3e] --> Energy [%.20f, %.20f]", rootNode.confToString(), pfuncLower.doubleValue(), pfuncUpper.doubleValue(), bc.freeEnergy(pfuncUpper),bc.freeEnergy(pfuncLower) ));
+            System.out.println(String.format("%s bounds: [%.3e, %.3e] --> Energy [%.20f, %.20f]", system.rootNode.confToString(), pfuncLower.doubleValue(), pfuncUpper.doubleValue(), system.bc.freeEnergy(pfuncUpper), system.bc.freeEnergy(pfuncLower)));
+        }
     }
 
     @Test
     public void testBoltzmannAndFreeEnergy(){
-        rootNode.index(confIndex);
-        BigDecimal pfuncUpper = lbScorer.calcCombinedScore(confIndex,rcs);
-        double energy = bc.freeEnergy(pfuncUpper);
-        BigDecimal weightedEnergy = bc.calc(energy);
-        double energy2 = bc.freeEnergy(weightedEnergy);
-        BigDecimal weightedEnergy2 = bc.calc(energy2);
+        for (TestContext system : systems) {
+            system.rootNode.index(system.confIndex);
+            BigDecimal pfuncUpper = system.lbScorer.calcCombinedScore(system.confIndex, system.rcs);
+            double energy = system.bc.freeEnergy(pfuncUpper);
+            BigDecimal weightedEnergy = system.bc.calc(energy);
+            double energy2 = system.bc.freeEnergy(weightedEnergy);
+            BigDecimal weightedEnergy2 = system.bc.calc(energy2);
 
-        System.out.println(String.format("Directly calculated pfunc: %.3e --> \n" +
-                "energy via bc.freeEnergy: %.3f --> \n" +
-                        "pfunc via bc.calc: %.3e --> \n" +
-                        "energy via bc.freeEnergy: %.3f --> \n" +
-                        "pfunc via bc.calc: %.3e",
-                pfuncUpper.doubleValue(), energy, weightedEnergy, energy2, weightedEnergy2));
+            System.out.println(String.format("Directly calculated pfunc: %.3e --> \n" +
+                            "energy via bc.freeEnergy: %.3f --> \n" +
+                            "pfunc via bc.calc: %.3e --> \n" +
+                            "energy via bc.freeEnergy: %.3f --> \n" +
+                            "pfunc via bc.calc: %.3e",
+                    pfuncUpper.doubleValue(), energy, weightedEnergy, energy2, weightedEnergy2));
 
-        BigDecimal testValue = BigDecimal.valueOf(7e75).setScale(64);
-        double testenergy = bc.freeEnergy(testValue);
-        BigDecimal testValue2 = bc.calc(testenergy);
-        System.out.println(String.format("test value: %.3e --> \n" +
-                        "energy via bc.freeEnergy: %.3f --> \n" +
-                        "pfunc via bc.calc: %.3e --> \n",
-                testValue, testenergy, testValue2));
+            BigDecimal testValue = BigDecimal.valueOf(7e75).setScale(64);
+            double testenergy = system.bc.freeEnergy(testValue);
+            BigDecimal testValue2 = system.bc.calc(testenergy);
+            System.out.println(String.format("test value: %.3e --> \n" +
+                            "energy via bc.freeEnergy: %.3f --> \n" +
+                            "pfunc via bc.calc: %.3e --> \n",
+                    testValue, testenergy, testValue2));
+        }
     }
 
     @Test
     public void testLevel1Bound(){
-        BigDecimal pfuncSum_upper = BigDecimal.ZERO;
-        BigDecimal pfuncSum_lower = BigDecimal.ZERO;
-        for (MultiSequenceSHARKStarNode.Node node : levelOne){
-            node.index(confIndex);
-            double g_lb = lbGScorer.calc(confIndex, rcs);
-            double g_ub = ubGScorer.calc(confIndex, rcs);
-            double h_lb = lbScorer.calc(confIndex,rcs);
-            double h_ub = ubScorer.calc(confIndex,rcs);
-            double lb = g_lb + h_lb;
-            double ub = g_ub + h_ub;
-            System.out.println(String.format("%s bounds: [%.3e, %.3e] --> Energy [%.3f, %.3f] Gscore: [%.3f, %.3f] Hscore: [%.3f, %.3f]",
-                    node.confToString(), bc.calc(ub), bc.calc(lb), lb, ub, g_lb, g_ub, h_lb, h_ub));
-            pfuncSum_upper = pfuncSum_upper.add(bc.calc(lb));
-            pfuncSum_lower = pfuncSum_lower.add(bc.calc(ub));
+        for (TestContext system : systems) {
+            BigDecimal pfuncSum_upper = BigDecimal.ZERO;
+            BigDecimal pfuncSum_lower = BigDecimal.ZERO;
+            List<MultiSequenceSHARKStarNode.Node> levelOne = getLevelOne(system, system.rootNode);
+            for (MultiSequenceSHARKStarNode.Node node : levelOne) {
+                node.index(system.confIndex);
+                double g_lb = system.lbGScorer.calc(system.confIndex, system.rcs);
+                double g_ub = system.ubGScorer.calc(system.confIndex, system.rcs);
+                double h_lb = system.lbScorer.calc(system.confIndex, system.rcs);
+                double h_ub = system.ubScorer.calc(system.confIndex, system.rcs);
+                double lb = g_lb + h_lb;
+                double ub = g_ub + h_ub;
+                System.out.println(String.format("%s bounds: [%.3e, %.3e] --> Energy [%.3f, %.3f] Gscore: [%.3f, %.3f] Hscore: [%.3f, %.3f]",
+                        node.confToString(), system.bc.calc(ub), system.bc.calc(lb), lb, ub, g_lb, g_ub, h_lb, h_ub));
+                pfuncSum_upper = pfuncSum_upper.add(system.bc.calc(lb));
+                pfuncSum_lower = pfuncSum_lower.add(system.bc.calc(ub));
+            }
+            System.out.println(String.format("Level 1 sum bounds: [%.3e, %.3e]", pfuncSum_lower.doubleValue(), pfuncSum_upper.doubleValue()));
         }
-        System.out.println(String.format("Level 1 sum bounds: [%.3e, %.3e]", pfuncSum_lower.doubleValue(), pfuncSum_upper.doubleValue()));
     }
 
     @Test
     public void testLevel1Bound_allatonce(){
-        BigDecimal pfuncSum_upper = BigDecimal.ZERO;
-        BigDecimal pfuncSum_lower = BigDecimal.ZERO;
-        for (MultiSequenceSHARKStarNode.Node node : levelOne){
-            node.index(confIndex);
-            BigDecimal pfuncUpper = lbScorer.calcCombinedScore(confIndex, rcs);
-            BigDecimal pfuncLower = ubScorer.calcCombinedScore(confIndex, rcs);
-            System.out.println(String.format("%s bounds: [%.3e, %.3e] --> Energy [%.3f, %.3f]",
-                    node.confToString(), pfuncLower, pfuncUpper, bc.freeEnergy(pfuncUpper), bc.freeEnergy(pfuncLower)));
-            pfuncSum_upper = pfuncSum_upper.add(pfuncUpper);
-            pfuncSum_lower = pfuncSum_lower.add(pfuncLower);
+        for (TestContext system : systems) {
+            BigDecimal pfuncSum_upper = BigDecimal.ZERO;
+            BigDecimal pfuncSum_lower = BigDecimal.ZERO;
+            List<MultiSequenceSHARKStarNode.Node> levelOne = getLevelOne(system, system.rootNode);
+            for (MultiSequenceSHARKStarNode.Node node : levelOne) {
+                node.index(system.confIndex);
+                BigDecimal pfuncUpper = system.lbScorer.calcCombinedScore(system.confIndex, system.rcs);
+                BigDecimal pfuncLower = system.ubScorer.calcCombinedScore(system.confIndex, system.rcs);
+                System.out.println(String.format("%s bounds: [%.3e, %.3e] --> Energy [%.3f, %.3f]",
+                        node.confToString(), pfuncLower, pfuncUpper, system.bc.freeEnergy(pfuncUpper), system.bc.freeEnergy(pfuncLower)));
+                pfuncSum_upper = pfuncSum_upper.add(pfuncUpper);
+                pfuncSum_lower = pfuncSum_lower.add(pfuncLower);
+            }
+            System.out.println(String.format("Level 1 sum bounds: [%.3e, %.3e]", pfuncSum_lower.doubleValue(), pfuncSum_upper.doubleValue()));
         }
-        System.out.println(String.format("Level 1 sum bounds: [%.3e, %.3e]", pfuncSum_lower.doubleValue(), pfuncSum_upper.doubleValue()));
     }
 
-    private class TestContext{
+    private static class TestContext{
         private String design;
         private String state;
         private int[] order;
+        private Map<String, String> mutations;
 
         private MultiSequenceSHARKStarNode.Node rootNode;
-        private List<MultiSequenceSHARKStarNode.Node> list;
 
         private SimpleConfSpace confSpace;
         private RCs rcs;
@@ -209,10 +166,17 @@ public class TestSHARKStarNodeScorer {
         private AStarScorer lbGScorer;
         private AStarScorer ubGScorer;
 
-        private TestContext( String design, String state, int[] order ){
+        private TestContext( String design, String state, int[] order, Map<String, String> mutations){
             this.design = design;
             this.state = state;
             this.order = order;
+            this.mutations = mutations;
+
+            try {
+                init();
+            }catch(FileNotFoundException e){
+                System.err.println(e.getMessage());
+            }
         }
 
         private void init() throws FileNotFoundException{
@@ -251,10 +215,13 @@ public class TestSHARKStarNodeScorer {
             EnergyMatrix minimizingEmat = new SimplerEnergyMatrixCalculator.Builder(minimizingConfEcalc).build().calcEnergyMatrix();
             EnergyMatrix correctionEmat = new UpdatingEnergyMatrix(confSpace, minimizingEmat);
 
-            Sequence wildType = confSpace.makeWildTypeSequence();
-            this.rcs = wildType.makeRCs(confSpace);
+            // Set the correct sequence
+            Sequence seq = confSpace.makeWildTypeSequence();
+            for (String entry : mutations.keySet()){
+                seq = seq.set(entry, mutations.get(entry));
+            }
+            this.rcs = seq.makeRCs(confSpace);
             rootNode = new MultiSequenceSHARKStarNode.Node(confSpace.positions.size(), 0, new MathTools.DoubleBounds());
-            levelOne = getLevelOne(rootNode);
 
             this.bc = new BoltzmannCalculator(new MathContext(BigDecimal.ROUND_HALF_UP));
             this.lbScorer = new SHARKStarNodeScorer(minimizingEmat, false);
