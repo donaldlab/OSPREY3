a423db0b805fa8654d0a9936b6f88ac128444531
Jeff Martin <jeff@cuchazinteractive.com>
2020-04-03 00:21:41 -0400

    oh right, we require JDK 11+ now, so let the Java compiler use the new features

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,8 +73,8 @@ repositories {
 }
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_1_8
-	targetCompatibility = JavaVersion.VERSION_1_8
+	sourceCompatibility = JavaVersion.VERSION_11
+	targetCompatibility = JavaVersion.VERSION_11
 	sourceSets {
 		get("main").apply {
 			java.srcDir("src")
