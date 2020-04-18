47faf780d6088a3ca5933489d22d5eb5da9fd741
Jonathan <jj@cs.duke.edu>
2020-04-18 10:54:41 -0400

    Add COVID Tests to cluster lab.

--- test/edu/duke/cs/osprey/ClusterLab.java
+++ test/edu/duke/cs/osprey/ClusterLab.java
@@ -3,10 +3,12 @@ package edu.duke.cs.osprey;
 import edu.duke.cs.osprey.astar.conf.ConfAStarTree;
 import edu.duke.cs.osprey.confspace.Sequence;
 import edu.duke.cs.osprey.confspace.SimpleConfSpace;
+import edu.duke.cs.osprey.confspace.Strand;
 import edu.duke.cs.osprey.ematrix.EnergyMatrix;
 import edu.duke.cs.osprey.ematrix.SimpleReferenceEnergies;
 import edu.duke.cs.osprey.ematrix.SimplerEnergyMatrixCalculator;
 import edu.duke.cs.osprey.energy.*;
+import edu.duke.cs.osprey.energy.forcefield.ForcefieldParams;
 import edu.duke.cs.osprey.kstar.KStar;
 import edu.duke.cs.osprey.kstar.KStarScoreWriter;
 import edu.duke.cs.osprey.kstar.TestKStar;
@@ -14,6 +16,9 @@ import edu.duke.cs.osprey.kstar.pfunc.GradientDescentPfunc;
 import edu.duke.cs.osprey.kstar.pfunc.PartitionFunction;
 import edu.duke.cs.osprey.parallelism.Cluster;
 import edu.duke.cs.osprey.parallelism.Parallelism;
+import edu.duke.cs.osprey.restypes.ResidueTemplateLibrary;
+import edu.duke.cs.osprey.structure.Molecule;
+import edu.duke.cs.osprey.structure.PDBIO;
 import edu.duke.cs.osprey.tools.Stopwatch;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
@@ -29,6 +34,13 @@ import static edu.duke.cs.osprey.tools.Log.log;
 
 
 public class ClusterLab {
+
+	enum SourceConfSpace {
+		Test2RL0,
+		TestCOVIDSmall,
+		TestCOVIDActual,
+
+	}
 
 	public static void main(String[] args)
 	throws Exception {
@@ -132,7 +144,19 @@ public class ClusterLab {
 		Parallelism parallelism = Parallelism.makeCpu(4);
 
 		// set up a toy design
-		TestKStar.ConfSpaces confSpaces = TestKStar.make2RL0();
+		TestKStar.ConfSpaces confSpaces = null;
+		SourceConfSpace source = SourceConfSpace.Test2RL0;
+		switch (source) {
+			case Test2RL0:
+			// set up a toy design
+			confSpaces = TestKStar.make2RL0();
+			break;
+			case TestCOVIDSmall:
+			confSpaces = TestCOVID.makeCOVIDSmall();
+			break;
+			case TestCOVIDActual:
+			confSpaces = TestCOVID.makeMakeCOVIDActual();
+		}
 
 		try (EnergyCalculator ecalc = new EnergyCalculator.Builder(confSpaces.asList(), confSpaces.ffparams)
 			.setCluster(cluster)
@@ -217,4 +241,114 @@ public class ClusterLab {
 			log("CLIENT: finished in %s", stopwatch.stop().getTime(2));
 		}
 	}
+
+	private static class TestCOVID {
+
+		public static TestKStar.ConfSpaces makeCOVIDSmall() {
+			TestKStar.ConfSpaces confSpaces = new TestKStar.ConfSpaces();
+
+			// configure the forcefield
+			confSpaces.ffparams = new ForcefieldParams();
+
+			Molecule mol = PDBIO.readResource("test-resources/ACE2_1r4l_peptide5vay_complex_model_leidosprep_declashed4.pdb");
+
+			// make sure all strands share the same template library
+			ResidueTemplateLibrary templateLib = new ResidueTemplateLibrary.Builder(confSpaces.ffparams.forcefld).build();
+
+			// define the protein strand
+			Strand protein = new Strand.Builder(mol)
+					.setTemplateLibrary(templateLib)
+					.setResidues("B123", "B139")
+					.build();
+			protein.flexibility.get("B125").setLibraryRotamers(Strand.WildType, "ARG", "TRP", "PHE", "TYR", "LYS", "HIP", "HIE", "HID", "MET", "ASP", "ASN", "GLU", "GLN").addWildTypeRotamers().setContinuous();
+			protein.flexibility.get("B129").setLibraryRotamers(Strand.WildType).addWildTypeRotamers().setContinuous();
+
+			// define the ligand strand
+			Strand ligand = new Strand.Builder(mol)
+					.setTemplateLibrary(templateLib)
+					.setResidues("A19", "A615")
+					.build();
+			ligand.flexibility.get("A329").setLibraryRotamers(Strand.WildType).addWildTypeRotamers().setContinuous();
+			ligand.flexibility.get("A330").setLibraryRotamers(Strand.WildType).addWildTypeRotamers().setContinuous();
+
+			// make the conf spaces ("complex" SimpleConfSpace, har har!)
+			confSpaces.protein = new SimpleConfSpace.Builder()
+					.addStrand(protein)
+					.build();
+			confSpaces.ligand = new SimpleConfSpace.Builder()
+					.addStrand(ligand)
+					.build();
+			confSpaces.complex = new SimpleConfSpace.Builder()
+					.addStrands(protein, ligand)
+					.build();
+
+			return confSpaces;
+		}
+
+		public static TestKStar.ConfSpaces makeMakeCOVIDActual() {
+			TestKStar.ConfSpaces confSpaces = new TestKStar.ConfSpaces();
+
+			// configure the forcefield
+			confSpaces.ffparams = new ForcefieldParams();
+
+			Molecule mol = PDBIO.readResource("test-resources/ACE2_1r4l_peptide5vay_complex_model_leidosprep_declashed4.pdb");
+
+			// make sure all strands share the same template library
+			ResidueTemplateLibrary templateLib = new ResidueTemplateLibrary.Builder(confSpaces.ffparams.forcefld).build();
+
+			// define the protein strand
+			Strand protein = new Strand.Builder(mol)
+					.setTemplateLibrary(templateLib)
+					.setResidues("B123", "B139")
+					.build();
+			// mutable
+			protein.flexibility.get("B123").setLibraryRotamers("ARG").setContinuous();
+			protein.flexibility.get("B125").setLibraryRotamers("HIP").setContinuous();
+			protein.flexibility.get("B126").setLibraryRotamers("HIP").addWildTypeRotamers().setContinuous();
+			protein.flexibility.get("B129").setLibraryRotamers("ARG").addWildTypeRotamers().setContinuous();
+			protein.flexibility.get("B130").setLibraryRotamers("ARG").addWildTypeRotamers().setContinuous();
+			protein.flexibility.get("B133").setLibraryRotamers("THR").addWildTypeRotamers().setContinuous();
+			protein.flexibility.get("B134").setLibraryRotamers("HIP").addWildTypeRotamers().setContinuous();
+			protein.flexibility.get("B136").setLibraryRotamers("ARG").addWildTypeRotamers().setContinuous();
+			protein.flexibility.get("B138").setLibraryRotamers("ARG").addWildTypeRotamers().setContinuous();
+			// flexible
+			protein.flexibility.get("B124").setLibraryRotamers(Strand.WildType).addWildTypeRotamers().setContinuous();
+			protein.flexibility.get("B139").setLibraryRotamers(Strand.WildType).addWildTypeRotamers().setContinuous();
+
+			// define the ligand strand
+			Strand ligand = new Strand.Builder(mol)
+					.setTemplateLibrary(templateLib)
+					.setResidues("A19", "A615")
+					.build();
+			ligand.flexibility.get("A34").setLibraryRotamers(Strand.WildType).addWildTypeRotamers().setContinuous();
+			ligand.flexibility.get("A35").setLibraryRotamers(Strand.WildType).addWildTypeRotamers().setContinuous();
+			ligand.flexibility.get("A37").setLibraryRotamers(Strand.WildType).addWildTypeRotamers().setContinuous();
+			ligand.flexibility.get("A38").setLibraryRotamers(Strand.WildType).addWildTypeRotamers().setContinuous();
+			ligand.flexibility.get("A41").setLibraryRotamers(Strand.WildType).addWildTypeRotamers().setContinuous();
+			ligand.flexibility.get("A42").setLibraryRotamers(Strand.WildType).addWildTypeRotamers().setContinuous();
+			ligand.flexibility.get("A45").setLibraryRotamers(Strand.WildType).addWildTypeRotamers().setContinuous();
+			ligand.flexibility.get("A49").setLibraryRotamers(Strand.WildType).addWildTypeRotamers().setContinuous();
+			ligand.flexibility.get("A329").setLibraryRotamers(Strand.WildType).addWildTypeRotamers().setContinuous();
+			ligand.flexibility.get("A330").setLibraryRotamers(Strand.WildType).addWildTypeRotamers().setContinuous();
+			ligand.flexibility.get("A352").setLibraryRotamers(Strand.WildType).addWildTypeRotamers().setContinuous();
+			ligand.flexibility.get("A353").setLibraryRotamers(Strand.WildType).addWildTypeRotamers().setContinuous();
+			ligand.flexibility.get("A354").setLibraryRotamers(Strand.WildType).addWildTypeRotamers().setContinuous();
+			ligand.flexibility.get("A355").setLibraryRotamers(Strand.WildType).addWildTypeRotamers().setContinuous();
+			ligand.flexibility.get("A357").setLibraryRotamers(Strand.WildType).addWildTypeRotamers().setContinuous();
+
+			// make the conf spaces ("complex" SimpleConfSpace, har har!)
+			confSpaces.protein = new SimpleConfSpace.Builder()
+					.addStrand(protein)
+					.build();
+			confSpaces.ligand = new SimpleConfSpace.Builder()
+					.addStrand(ligand)
+					.build();
+			confSpaces.complex = new SimpleConfSpace.Builder()
+					.addStrands(protein, ligand)
+					.build();
+
+			return confSpaces;
+		}
+	}
 }
+
