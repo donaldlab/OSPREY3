18c8b47eaadeb9af7ff7000da40b6de3787ebe0b
Nate Guerin <nate@textpla.in>
2023-05-08 01:48:25 -0400

    install java so the correct JDK can be downloaded

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,5 @@
 rootProject.name = "osprey3"
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.5.0")
 }
