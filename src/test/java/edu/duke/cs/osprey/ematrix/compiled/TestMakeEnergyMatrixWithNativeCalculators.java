28f5bb64b79628600423e22272a3c77906e46985
Nate Guerin <nate@textpla.in>
2023-12-17 16:22:44 -0500

    Add min repro test for NativeConfEnergyCalc failure
    Calculation of an energy matrix using the NativeConfEnergyCalculator
    fails when one of the bodies has translation and rotation enabled. This
    PR adds unit tests with minimal working examples to reproduce the error.
    The example is a conformation space created from PDB 6ov7 (CALP:kCAL01)
    with no mutations on either molecule, no amino acid flexibility on
    either molecule, and translation+rotation on kCAL01.

---
+++ src/test/java/edu/duke/cs/osprey/ematrix/compiled/TestMakeEnergyMatrixWithNativeCalculators.java
@@ -0,0 +1,50 @@
+package edu.duke.cs.osprey.ematrix.compiled;
+
+import edu.duke.cs.osprey.confspace.compiled.ConfSpace;
+import edu.duke.cs.osprey.energy.compiled.CPUConfEnergyCalculator;
+import edu.duke.cs.osprey.energy.compiled.ConfEnergyCalculator;
+import edu.duke.cs.osprey.energy.compiled.CudaConfEnergyCalculator;
+import edu.duke.cs.osprey.energy.compiled.NativeConfEnergyCalculator;
+import edu.duke.cs.osprey.gpu.Precision;
+import edu.duke.cs.osprey.tools.FileTools;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+public class TestMakeEnergyMatrixWithNativeCalculators {
+    private static final ConfSpace confSpace = ConfSpace.fromBytes(FileTools.readResourceBytes("/confSpaces/6og7.transrot.minimal.ccsx"));
+
+    private void testCanMakeEnergyMatrixWithCalculator(Supplier<ConfEnergyCalculator> calculatorSupplier) {
+        try (var calculator = calculatorSupplier.get()) {
+            var emat = new EmatCalculator.Builder(calculator)
+                    .build()
+                    .calc();
+
+            assertNotNull(emat);
+        }
+    }
+
+    @Test
+    public void testCanMakeEnergyMatrixWithCpuCalculator() {testCanMakeEnergyMatrixWithCalculator(() -> new CPUConfEnergyCalculator(confSpace));}
+
+    @Test
+    public void testCanMakeEnergyMatrixWithNative64Calculator() { testCanMakeEnergyMatrixWithCalculator(() -> new NativeConfEnergyCalculator(confSpace, Precision.Float64)); }
+
+    @Test
+    public void testCanMakeEnergyMatrixWithNative32Calculator() { testCanMakeEnergyMatrixWithCalculator(() -> new NativeConfEnergyCalculator(confSpace, Precision.Float32)); }
+
+    @Test
+    public void testCanMakeEnergyMatrixWithCuda64Calculator() {
+        assumeTrue(CudaConfEnergyCalculator.isSupported());
+        testCanMakeEnergyMatrixWithCalculator(() -> new CudaConfEnergyCalculator(confSpace, Precision.Float64));
+    }
+
+    @Test
+    public void testCanMakeEnergyMatrixWithCuda32Calculator() {
+        assumeTrue(CudaConfEnergyCalculator.isSupported());
+        testCanMakeEnergyMatrixWithCalculator(() -> new CudaConfEnergyCalculator(confSpace, Precision.Float32));
+    }
+}
