6d8a04727934b9c2b59d0d7b5781af8b919a4dc5
henry-childs <henry.childs@duke.edu>
2024-09-10 12:41:24 -0400

    Added feature for skipping disjoint segments. Also, MASTER returns don't need to be aligned (BioPython superimpose), because MASTER preserves the origin

--- examples/ccs.D-peptide-L-protein/Scaffold_Generator.py
+++ examples/ccs.D-peptide-L-protein/Scaffold_Generator.py
@@ -11,8 +11,9 @@ import os
 # input_directory is the MASTER matches returns (all PDBs)
 # output_directory is where to put the D-L complexes
 
-# hide warnings
+# hide warnings - throws for a lot of atom names, but ok
 warnings.simplefilter('ignore')
+
 
 def scaffold_generator(complex_pdb: str, input_directory: str, output_directory: str):
 
@@ -26,42 +27,41 @@ def scaffold_generator(complex_pdb: str, input_directory: str, output_directory:
         # get the old (L-space) peptide
         old_peptide = complex_model["y"]
 
-        # flip each MASTER L-match to D-space, and change ID if needed
+        # get the MASTER L-peptide
         full_pdb_path = os.path.join(input_directory, pdb_path)
         M_pep_structure = parser.get_structure('match', full_pdb_path)
-
         M_peptide_model = M_pep_structure[0]
+
+        # skip the match if it has a disjoint segment
+        has_disjoint = False
+        last_res = 0
+        for chain in M_peptide_model:
+            for res in chain:
+                if last_res == 0:
+                    last_res = res.id[1]
+                elif (res.id[1] - last_res) != 1:
+                    has_disjoint = True
+                    print("SKIP: %s has a disjoint segment and will not be prepared" % pdb_path)
+                    break
+                else:
+                    last_res = res.id[1]
+        if has_disjoint:
+            continue
+
+        # flip each MASTER L-match to D-space, and change ID if needed
         for chain in M_peptide_model:
             if chain.id == "z" or chain.id == "y":
                 chain.id = "A"
-                print("%s had the same chain ID as the complex, so changed to A" % pdb_path)
+                print("ALTERATION: %s had the same chain ID as the complex, so changed ID to A" % pdb_path)
             for res in chain:
                 for a in res:
                     a.coord[2] = a.coord[2] * -1
 
-        # align the old L-pep to the MASTER L-pep
-        # get the CA from both
-        old_atoms = []
-        M_atoms = []
-        for res in old_peptide:
-            for a in res:
-                if a.name == 'CA':
-                    old_atoms.append(a)
-        for chain in M_peptide_model:
-            for res in chain:
-                for a in res:
-                    if a.name == 'CA':
-                        M_atoms.append(a)
-
-        imposer = Superimposer()
-        imposer.set_atoms(old_atoms, M_atoms)
-        imposer.apply(M_peptide_model.get_atoms())
-
-        # add to new D-pep to the PDB
+        # MASTER already aligns, so just add the D-pep
         for chain in M_peptide_model:
             complex_model.add(chain)
 
-        # remove the old peptide from the complex
+        # remove the old L-peptide from the complex
         complex_model.detach_child("y")
 
         # print new PDB to output_directory
@@ -70,3 +70,10 @@ def scaffold_generator(complex_pdb: str, input_directory: str, output_directory:
         pdb_name = pdb_path[:-4] + "-complex.pdb"
         savename = os.path.join(output_directory, pdb_name)
         io.save(savename)
+
+        # update terminal
+        print("------ completed %s ------" % pdb_path)
+
+
+# example call
+# scaffold_generator("8gaj-trim-renamed.pdb", "matches", "scaffolds")
